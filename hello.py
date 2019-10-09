925a7fc884b798614eaed1a851988a1200578072
JadeMcVay <jademcvay@gmail.com>
2019-10-09 13:42:54 -0700

    I made this while eating Taco Bell

--- hello.py
+++ hello.py
@@ -1,15 +1,3 @@
 Hello. 
 
-<<<<<<< HEAD
-My name is Jade and I'm a fourth year BioEngineering major. 
-||||||| merged common ancestors
 x = 5
-
-y = 10
-=======
-x = 5
-
-y = 10
-
-z = 14
->>>>>>> 586c6a0467cd0f9426395be023ac7ad200dc75ff
