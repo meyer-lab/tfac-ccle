23af4de3d1a33ee7a4ab66d3ea2df0310589d8af
Nilay S <nilay.shah19@gmail.com>
2019-10-10 14:09:15 -0700

    Fixing mistakes

--- Test.py
+++ Test.py
@@ -1,3 +1,3 @@
 import numpy as np
 x = np.zeros(10)
-"""Micah is so cool and sick and dear god I'm jealous of everything that he resembles"""
+"""Nilay is so cool and sick and dear god I'm jealous of everything that he resembles"""
