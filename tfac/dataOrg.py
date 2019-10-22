347cb43b7c578432e6bd0d1480e1d387655355ce
Nilay S <nilay.shah19@gmail.com>
2019-10-22 16:26:29 -0700

    Gene Alignment Done

--- tfac/dataOrg.py
+++ tfac/dataOrg.py
@@ -1,5 +1,5 @@
 import numpy as np
 import pandas as pd
 
-def extractData(filename, columns):
-    return pd.read_excel(filename, header = 2, index_col = 0, usecols = columns)
+def extractData(filename, columns = None, row = 0, col = None):
+    return pd.read_excel(filename, header = row, index_col = col, usecols = columns)
