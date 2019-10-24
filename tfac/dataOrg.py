b0155152fe7b4c169ecbdf9a3f969b4d2e32a0bb
Micah Bryant <micah.bryant14@gmail.com>
2019-10-24 14:46:11 -0700

    testing some shit

--- tfac/dataOrg.py
+++ tfac/dataOrg.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 def extractData(filename, columns = None, row = 0, col = None):
     return pd.read_excel(filename, header = row, index_col = col, usecols = columns)
+
+#def storeData(dataFile):
+ #   with 
