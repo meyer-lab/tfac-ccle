88ea568f13123ba1ce0a492c5eab14c7779dc13a
Robby <robcatdude@yahoo.com>
2019-10-25 12:40:16 -0700

    Please watch the whitespaces people

--- tfac/dataOrg.py
+++ tfac/dataOrg.py
@@ -7,22 +7,22 @@ def extractData(filename, columns=None, row=0, col=None):
     return pd.read_excel(filename, header=row, index_col=col, usecols=columns)
 
 def extractCopy(dupes=False):
-    ''' 
+    '''
     Extracts out all duplicates data using excel file of gene names
 
     Returns:
             Order: Methylation, Gene Expression, Copy Number
-            List of length 3 containing 3D arrays with 
+            List of length 3 containing 3D arrays with
             duplicate gene names, indices, and # of dupes corresponding to each name
             Also returns # of duplicates in each data set
     '''
     data = extractData('data/GeneData_All.xlsx', 'A:C')
     data = data.to_numpy()
 
-    methylation = np.append(data[:12158,0],data[12159:21338,0])
-    geneExp = data[:,1]
-    copyNum = data[:23316,2]
-    data = [methylation.astype(str),geneExp.astype(str),copyNum.astype(str)]
+    methylation = np.append(data[:12158,0], data[12159:21338,0])
+    geneExp = data[:, 1]
+    copyNum = data[:23316, 2]
+    data = [methylation.astype(str), geneExp.astype(str), copyNum.astype(str)]
 
     if dupes:
         duplicates = np.zeros(3)
@@ -42,7 +42,7 @@ def extractCopy(dupes=False):
                 copyData.append(uData[0][j])
                 idxData.append(uData[1][j])
                 count.append(uData[2][j])
-        returnVal.append(np.array([copyData,idxData,count]))
+        returnVal.append(np.array([copyData, idxData, count]))
 
     if dupes:
         return returnVal, duplicates
