905d74a71915e9a46e346d9b80fde811928d4d6d
JadeMcVay <jademcvay@gmail.com>
2019-11-14 21:43:55 -0800

    Calculations for missing values

--- tfac/dataHelpers.py
+++ tfac/dataHelpers.py
@@ -33,11 +33,11 @@ def importData(username, password, dataType=None):
         return None
 
     ## Find Data
-    if data == 'Copy Number All':
+    if dataType == 'Copy Number All':
         data = syn.get('syn21089502')
-    elif data == 'Methylation All':
+    elif dataType == 'Methylation All':
         data = syn.get('syn21089540')
-    elif data == 'Gene Expression All':
+    elif dataType == 'Gene Expression All':
         data = syn.get('syn21089539')
 
     df = pd.read_excel(data.path, index_col = 0)
