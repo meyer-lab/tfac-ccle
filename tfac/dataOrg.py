b4f6d9cf1468bb134760564358646f54ab45eaa1
Micah Bryant <micah.bryant14@gmail.com>
2020-02-23 11:03:38 -0800

    fun fun fun

--- tfac/dataOrg.py
+++ tfac/dataOrg.py
@@ -160,5 +160,3 @@ def extractCopy(dupes=False, cellLines=False):
         return returnVal, duplicates
     else:
         return returnVal
-
-# want to slice into array to create new datafile using indices
