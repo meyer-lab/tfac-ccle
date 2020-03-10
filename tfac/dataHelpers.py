a344de211b701360f8eb7ac45f0108af002b4801
robthemike1 <43423961+robthemike1@users.noreply.github.com>
2020-03-09 23:07:17 -0700

    Update dataHelpers.py

--- tfac/dataHelpers.py
+++ tfac/dataHelpers.py
@@ -108,7 +108,7 @@ def makeTensor(username, password, impute=False):
 def getCellLineComps(imputed=False):
     '''Import cell line components'''
     if imputed:
-        filename = os.path.join(path, './data/Imputed_Components_50.hdf5')
+        filename = os.path.join(path, './data/Imputed_Components_100.hdf5')
         with h5py.File(filename, 'r') as f:
             data = f["Cell_Line_Comps"][:]
             f.close()
@@ -124,7 +124,7 @@ def getCellLineComps(imputed=False):
 def getGeneComps(imputed=False):
     '''Import gene components --- rank 25 cp'''
     if imputed:
-        filename = os.path.join(path, './data/Imputed_Components_50.hdf5')
+        filename = os.path.join(path, './data/Imputed_Components_100.hdf5')
         with h5py.File(filename, 'r') as f:
             data = f["Gene_Comps"][:]
             f.close()
