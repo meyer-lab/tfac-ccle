078708a6647210835f895c69d414c4bc66770487
Scott T <sco.d.taylor@gmail.com>
2020-04-03 18:18:34 -0700

    another comment

--- tfac/dataOrg.py
+++ tfac/dataOrg.py
@@ -3,7 +3,7 @@ from functools import reduce
 import numpy as np
 import pandas as pd
 
-
+#another commment
 def extractData(filename, columns=None, row=0, col=None):
     '''useless -- to be deleted'''
     return pd.read_excel(filename, header=row, index_col=col, usecols=columns)
