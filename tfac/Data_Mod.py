7b19816fa270fd8fb21bef978d541a6391b0f59c
Robby <robcatdude@yahoo.com>
2020-04-10 16:02:35 -0700

    Quick Fixes

--- tfac/Data_Mod.py
+++ tfac/Data_Mod.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-from dataHelpers import importLINCSprotein
+from .dataHelpers import importLINCSprotein
 
 def data_mod(x):
+    '''Creates a slice of the data tensor corresponding to the inputted treatment'''
     df = importLINCSprotein()
-    spec_df = ds.loc[(ds['Treatment'] == 'Control') | (ds['Treatment'] == x)]
+    spec_df = df.loc[(df['Treatment'] == 'Control') | (df['Treatment'] == x)]
     spec_df = spec_df.drop(columns = ['Sample description', 'Treatment', 'File'])
     y = spec_df.to_numpy()
-    return y
+    return y, spec_df
