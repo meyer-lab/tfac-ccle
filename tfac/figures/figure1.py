8089b6f143788b1f6d0d41695ca26687668ac680
Robby <robcatdude@yahoo.com>
2020-04-12 22:08:38 -0700

    Finalize Figure 1

--- tfac/figures/figure1.py
+++ tfac/figures/figure1.py
@@ -10,8 +10,9 @@ from ..tensor import perform_decomposition, find_R2X, find_R2X_nnp
 import matplotlib.pyplot as plt
 
 
-ts, junk1, junk2 = form_tensor()
-factors = perform_decomposition(ts, 2)
+tensor, treatments, times = form_tensor()
+factors = perform_decomposition(tensor, 2)
+
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
@@ -19,10 +20,10 @@ def makeFigure():
 
     ax[0].axis('off')  # blank out axes for cartoon
     R2X_figure(ax[1])
-    cellLinePlot(ax[2], factors[0], 1,2, junk1)
-    cellLinePlotTime(ax[3], factors[1], 1,2, junk2)
-    cellLinePlot(ax[4], factors[2], 1,2, None)
-    
+    treatmentPlot(ax[2], factors[0], 1, 2, treatments)
+    timePlot(ax[3], factors[1], 1, 2, times)
+    proteinPlot(ax[4], factors[2], 1, 2)
+
     # Add subplot labels
     subplotLabel(ax)
 
@@ -36,20 +37,19 @@ def R2X_figure(ax):
     for i in nComps:
         R2X.append(find_R2X_nnp(form_tensor()[0],i))
     ax = sns.scatterplot(nComps, R2X, ax=ax)
-    sns.despine(ax=ax)
     ax.set_xlabel("Rank Decomposition")
     ax.set_ylabel("R2X")
     ax.set_title("CP Decomposition")
 
 
-def cellLinePlot(ax, factors, r1, r2, senthue):
-    '''Plot Cell Lines (tensor axis 0) in factorization component space'''
+def treatmentPlot(ax, factors, r1, r2, senthue):
+    '''Plot Treatment (tensor axis 0) in factorization component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, hue=senthue)
     ax.set_xlabel('Component ' + str(r1))
     ax.set_ylabel('Component ' + str(r2))
     ax.set_title('Treatment Factors')
     
-def cellLinePlotTime(ax, factors, r1, r2, senthue):
+def timePlot(ax, factors, r1, r2, senthue):
     '''Plot Cell Lines (tensor axis 0) in factorization component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, hue=senthue)
     ax.set_xlabel('Component ' + str(r1))
@@ -58,13 +58,12 @@ def cellLinePlotTime(ax, factors, r1, r2, senthue):
 
 
 
-def genePlot(ax, factors, r1, r2):
+def proteinPlot(ax, factors, r1, r2):
     '''Plot genes (tensor axis 1) in factorization component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax)
     ax.set_xlabel('Component ' + str(r1))
     ax.set_ylabel('Component ' + str(r2))
-    ax.set_title('Gene Factors')
-    setPlotLimits(ax, factors, r1, r2)
+    ax.set_title('Protein Factors')
 
 
 def characPlot(ax, factors, r1, r2):
