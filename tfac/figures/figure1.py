7df407ea62effe2a9ecb2fd6f7b7d3057a088820
Aaron Meyer <aaron@asmlab.org>
2020-04-20 14:12:14 -0700

    Revert "changed to means"
    This reverts commit 63fd3e75c97764536d974a87bdeba27307e7edcc, reversing
    changes made to c88d4ac0bab506b6726cb6bd46fadeb6f4e19ed0.

--- tfac/figures/figure1.py
+++ tfac/figures/figure1.py
@@ -5,7 +5,13 @@ import numpy as np
 import seaborn as sns
 from .figureCommon import subplotLabel, getSetup
 from ..dataHelpers import cellLineNames
+from ..Data_Mod import form_tensor
+from ..tensor import perform_decomposition, find_R2X_nnp
+import matplotlib.pyplot as plt
 
+
+tensor, treatments, times = form_tensor()
+factors = perform_decomposition(tensor, 2)
 
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
@@ -13,7 +19,10 @@ def makeFigure():
     ax, f = getSetup((7, 6), (3, 2))
 
     ax[0].axis('off')  # blank out axes for cartoon
-    ax[1].axis('off')
+    R2X_figure(ax[1])
+    treatmentPlot(ax[2], factors[0], 1, 2, treatments)
+    timePlot(ax[3], factors[1], 1, 2, times)
+    proteinPlot(ax[4], factors[2], 1, 2)
 
     # Add subplot labels
     subplotLabel(ax)
@@ -23,35 +32,40 @@ def makeFigure():
 
 def R2X_figure(ax):
     '''Create Parafac R2X Figure'''
-    ### THIS DATA COMES FROM MATLAB -- INSERT NEW DATA HERE/CALL FUNCTION HERE ###
-    nComps = [0, 1, 2, 3, 4, 5, 10, 15, 20, 25]
-    R2X = [0, .681, .744, .787, .805, .819, .861, .887, .904, .916]
+    R2X = []
+    nComps = range(1,11)
+    for i in nComps:
+        R2X.append(find_R2X_nnp(form_tensor()[0],i))
     ax = sns.scatterplot(nComps, R2X, ax=ax)
-    sns.despine(ax=ax)
     ax.set_xlabel("Rank Decomposition")
     ax.set_ylabel("R2X")
-    ax.set_xticks([0, 5, 10, 15, 20])
     ax.set_title("CP Decomposition")
 
 
-def cellLinePlot(ax, factors, r1, r2):
-    '''Plot Cell Lines (tensor axis 0) in factorization component space'''
-    sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, hue=cellLineNames(), legend=False)
+def treatmentPlot(ax, factors, r1, r2, senthue):
+    '''Plot Treatment (tensor axis 0) in factorization component space'''
+    sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, hue=senthue)
     ax.set_xlabel('Component ' + str(r1))
     ax.set_ylabel('Component ' + str(r2))
-    ax.set_title('Cell Line Factors')
-    setPlotLimits(ax, factors, r1, r2)
+    ax.set_title('Treatment Factors')
+    
+def timePlot(ax, factors, r1, r2, senthue):
+    '''Plot Cell Lines (tensor axis 0) in factorization component space'''
+    sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, hue=senthue)
+    ax.set_xlabel('Component ' + str(r1))
+    ax.set_ylabel('Component ' + str(r2))
+    ax.set_title('Time Factors')
 
-
-def genePlot(ax, factors, r1, r2):
+def proteinPlot(ax, factors, r1, r2):
     '''Plot genes (tensor axis 1) in factorization component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax)
     ax.set_xlabel('Component ' + str(r1))
     ax.set_ylabel('Component ' + str(r2))
-    ax.set_title('Gene Factors')
-    setPlotLimits(ax, factors, r1, r2)
+    ax.set_title('Protein Factors')
 
 
+############## From Capstone ###################################################################################################################    
+    
 def characPlot(ax, factors, r1, r2):
     '''Plot the measured genetic characteristics (tensor axis 2) in component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, style=['Gene Expression', 'Copy Number', 'Methylation'])
