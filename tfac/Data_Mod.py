c88d4ac0bab506b6726cb6bd46fadeb6f4e19ed0
Vedant Sathye <vedant.sathye@gmail.com>
2020-04-19 22:57:04 -0700

    changed to means

--- tfac/Data_Mod.py
+++ tfac/Data_Mod.py
@@ -8,21 +8,34 @@ def data_mod(x, df = None):
         df = importLINCSprotein()
     spec_df = df.loc[(df['Treatment'] == 'Control') | (df['Treatment'] == x)]
     times = spec_df['Time'].to_numpy().tolist()
-    spec_df = spec_df.drop(columns = ['Sample description', 'Treatment', 'File', 'Time'])
+    spec_df = spec_df.drop(columns = ['Sample description', 'Treatment', 'Time'])
     y = spec_df.to_numpy()
     return y, spec_df, times
 
 def form_tensor():
     '''Creates tensor in numpy array form and returns tensor, treatments, and time'''
     df = importLINCSprotein()
-
-    unique_treatments = np.unique(df['Treatment'].values).tolist()
+    temp = pd.DataFrame()
+    tempindex = df["Sample description"]
+    tempindex = tempindex[:36]
+    i = 0
+    for a in tempindex:
+        tempindex[i] = a[3:]
+        i+=1
+    treatments = df["Treatment"][0:36]
+    times = df["Time"][0:36]
+    df =  df.drop(["Sample description"], axis = 1)
+    by_row_index = df.groupby(df.index)
+    df_means = by_row_index.mean() 
+    df_means.insert(0,"Treatment",value = treatments)
+    df_means.insert(0, "Sample description", tempindex)
+    unique_treatments = np.unique(df_means['Treatment'].values).tolist()
     unique_treatments.remove('Control')
 
     slices = []
     df_slices = []
     for treatment in unique_treatments:
-        array, df_array, times = data_mod(treatment, df)
+        array, df_array, times = data_mod(treatment, df_means)
         slices.append(array)
     tensor = np.stack(slices)
     return tensor, unique_treatments, times
--- tfac/figures/figure1.py
+++ tfac/figures/figure1.py
@@ -6,12 +6,13 @@ import seaborn as sns
 from .figureCommon import subplotLabel, getSetup
 from ..dataHelpers import cellLineNames
 from ..Data_Mod import form_tensor
-from ..tensor import perform_decomposition, find_R2X, find_R2X_nnp
+from ..tensor import perform_decomposition, find_R2X_nnp
 import matplotlib.pyplot as plt
 
 
-ts, junk1, junk2 = form_tensor()
-factors = perform_decomposition(ts, 2)
+tensor, treatments, times = form_tensor()
+factors = perform_decomposition(tensor, 2)
+
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
@@ -19,10 +20,10 @@ def makeFigure():
 
     ax[0].axis('off')  # blank out axes for cartoon
     R2X_figure(ax[1])
-    cellLinePlot(ax[2], factors[0], 1,2, junk1)
-    cellLinePlotTime(ax[3], factors[1], 1,2, junk2)
-    cellLinePlot(ax[4], factors[2], 1,2, None)
-    
+    treatmentPlot(ax[2], factors[0], 1, 2, treatments)
+    timePlot(ax[3], factors[1], 1, 2, times)
+    proteinPlot(ax[4], factors[2], 1, 2)
+
     # Add subplot labels
     subplotLabel(ax)
 
@@ -36,37 +37,35 @@ def R2X_figure(ax):
     for i in nComps:
         R2X.append(find_R2X_nnp(form_tensor()[0],i))
     ax = sns.scatterplot(nComps, R2X, ax=ax)
-    sns.despine(ax=ax)
     ax.set_xlabel("Rank Decomposition")
     ax.set_ylabel("R2X")
     ax.set_title("CP Decomposition")
 
 
-def cellLinePlot(ax, factors, r1, r2, senthue):
-    '''Plot Cell Lines (tensor axis 0) in factorization component space'''
+def treatmentPlot(ax, factors, r1, r2, senthue):
+    '''Plot Treatment (tensor axis 0) in factorization component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, hue=senthue)
     ax.set_xlabel('Component ' + str(r1))
     ax.set_ylabel('Component ' + str(r2))
     ax.set_title('Treatment Factors')
     
-def cellLinePlotTime(ax, factors, r1, r2, senthue):
+def timePlot(ax, factors, r1, r2, senthue):
     '''Plot Cell Lines (tensor axis 0) in factorization component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, hue=senthue)
     ax.set_xlabel('Component ' + str(r1))
     ax.set_ylabel('Component ' + str(r2))
     ax.set_title('Time Factors')
 
-
-
-def genePlot(ax, factors, r1, r2):
+def proteinPlot(ax, factors, r1, r2):
     '''Plot genes (tensor axis 1) in factorization component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax)
     ax.set_xlabel('Component ' + str(r1))
     ax.set_ylabel('Component ' + str(r2))
-    ax.set_title('Gene Factors')
-    setPlotLimits(ax, factors, r1, r2)
+    ax.set_title('Protein Factors')
 
 
+############## From Capstone ###################################################################################################################    
+    
 def characPlot(ax, factors, r1, r2):
     '''Plot the measured genetic characteristics (tensor axis 2) in component space'''
     sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax, style=['Gene Expression', 'Copy Number', 'Methylation'])
