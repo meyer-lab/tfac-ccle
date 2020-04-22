3543da61628f4c48aea7b8ed28b0a86a47af804b
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-04-22 06:41:09 -0700

    Update tensor.py

--- tfac/tensor.py
+++ tfac/tensor.py
@@ -24,6 +24,26 @@ def R2X(reconstructed, original):
     """ Calculates R2X of two tensors. """
     return 1.0 - tl_var(reconstructed - original) / tl_var(original)
 
+
+def reorient_one(factors, component_index):
+    """ Takes the factor matrices and flips factor one with its next if negative. """
+
+    for index in range(len(factors) - 1):
+       meann = np.mean(factors[index][:, component_index])
+        if meann < 0:
+            factors[index][:, component_index] *= -1
+            factors[index + 1][:, component_index] *= -1
+
+    return factors
+
+
+def reorient_factors(factors):
+    """ This function is to reorient the factors if at least one component in two factors matrices are negative. """
+    for jj in range(factors[0].shape[1]):
+        factors = reorient_one(factors, jj)
+    return factors
+
+
 #### Decomposition Methods ###################################################################
 
 def cp_decomp(tensor, r, nneg=False):
