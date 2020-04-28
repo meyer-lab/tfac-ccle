ad745bbe23b139c68fe3022893944c511a0334cb
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-04-28 14:18:39 -0700

    Update figure2.py

--- tfac/figures/figure2.py
+++ tfac/figures/figure2.py
@@ -9,7 +9,7 @@ from ..Data_Mod import form_tensor
 from ..tensor import tucker_decomp, find_R2X_tucker
 
 tensor, treatments, times = form_tensor()
-results = tucker_decomp(tensor, (4, 4, 5))
+results = tucker_decomp(tensor, (5, 4, 10))
 factors = results[1]
 print("Tucker R2X: " + str(find_R2X_tucker(results, tensor)))
 
@@ -34,7 +34,7 @@ def R2X_figure(ax):
     R2X = np.zeros(14)
     nComps = range(1, len(R2X))
     for i in nComps:
-        output = tucker_decomp(tensor, (i, i, i))
+        output = tucker_decomp(tensor, (5, 4, i))
         R2X[i] = find_R2X_tucker(output, tensor)
     sns.scatterplot(np.arange(len(R2X)), R2X, ax=ax)
     ax.set_xlabel("Rank Decomposition")
