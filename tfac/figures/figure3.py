427803a80bd613c859f9231703e4e5d552cb9adb
Aditya Sivakumar <asivakumar11@g.ucla.edu>
2020-04-30 14:49:15 -0700

    all

--- tfac/figures/figure3.py
+++ tfac/figures/figure3.py
@@ -22,6 +22,10 @@ def makeFigure():
     treatmentPlot(ax[1], factors[0], 2,3, treatments)
     treatmentPlot(ax[2], factors[0], 3,4, treatments)
     treatmentPlot(ax[3], factors[0], 4,5, treatments)
+    treatmentPlot(ax[4], factors[0], 5,6, treatments)
+    treatmentPlot(ax[5], factors[0], 6,7, treatments)
+    treatmentPlot(ax[6], factors[0], 7,8, treatments)
+    treatmentPlot(ax[7], factors[0], 8,9, treatments)
     #timePlot(ax[2], factors[1])
     #proteinPlot(ax[3], factors[2], 1, 2)
 
