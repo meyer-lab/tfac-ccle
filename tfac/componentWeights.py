40ddb2bc5c8b497b452d23207e440f65dbf8331c
Your Name <you@example.com>
2020-04-30 02:04:49 -0700

    Updated componentsWeight function to add topThreeProteins

--- tfac/componentWeights.py
+++ tfac/componentWeights.py
@@ -10,23 +10,21 @@ def proteinNames():
     return proteinN 
     
 def componentWeights(decomps, proteinLabels):
-    #takes in array of protein decomp'd components and list of proteinNames and picks out top 3 protein weights
-    #components are by columns, proteins by rows
-    i = np.shape(decomps)
-    proteins = decomps[i[0]-1]
-    proteinNum, compNum = i
-    p_weights = np.zeros((3, compNum)) #stores indicies of 3 largest protein weights
-    p_labels = [] #corresponding labels
-    p_labels.append([])
-    p_labels.append([])
-    p_labels.append([])
-    p_sorted=np.argsort(proteins,axis=0) #sorts by component from smallest to largest
+    '''takes in array of protein decomp'd components and list of proteinNames and picks out top 3 protein weights
+    components are by columns, proteins by rows'''
+    i=np.shape(decomps)
+    proteins=decomps[i[0]-1]
+    proteinNum,compNum=np.shape(proteins)
+    p_weights=np.zeros((3,compNum)) '''stores indicies of 3 largest protein weights'''
+    p_labels=[] '''corresponding labels'''
+
+    p_sorted=np.argsort(proteins,axis=0) '''indirectly sorts by component from smallest to largest'''
     
     for y in range(0, compNum):
         for x in range(0, 3):
             p_weights[x,y] = p_sorted[proteinNum-(x+1),y]
-            ind = int(p_weights[x,y])
-            p_labels[x].append(proteinLabels[ind])
+
+        p_labels.append(topThreeProteins(proteins[:,y]))
         
     return p_weights, p_labels
 
@@ -36,5 +34,5 @@ def topThreeProteins(comp):
     for i in range(0, 3):
         temp = np.argmax(comp)
         names.append(proteinNames()[temp])
-        comp[temp] = -1000
+        comp[temp] = -100000
     return names
