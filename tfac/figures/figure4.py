f4c02b2615e8e04e33154bb81862a3c30da25b55
Aditya Sivakumar <asivakumar11@g.ucla.edu>
2020-05-02 12:15:57 -0700

    protein plots

--- tfac/figures/figure4.py
+++ tfac/figures/figure4.py
@@ -1,17 +1,32 @@
 """
-This creates Figure 4.
+This creates Figure 4 - All Tucker protein component plots
 """
+import numpy as np
+import seaborn as sns
 from .figureCommon import subplotLabel, getSetup
+from .figure1 import treatmentPlot, timePlot, proteinPlot
+from ..Data_Mod import form_tensor
+from ..tensor import tucker_decomp
+
+tensor, treatments, times = form_tensor()
+results = tucker_decomp(tensor, (9, 9, 9))
+factors1 = results[1]
 
 
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
-    ax, f = getSetup((7.5, 6), (3, 4))
+    ax, f = getSetup((7, 6), (3, 3))
 
-    ax[0].axis('off')  # blank out first axis for cartoon
-
+    proteinPlot(ax[0], factors1[2], 1, 2)
+    proteinPlot(ax[1], factors1[2], 2, 3)
+    proteinPlot(ax[2], factors1[2], 3, 4)
+    proteinPlot(ax[3], factors1[2], 4, 5)
+    proteinPlot(ax[4], factors1[2], 5, 6)
+    proteinPlot(ax[5], factors1[2], 6, 7)
+    proteinPlot(ax[6], factors1[2], 7, 8)
+    proteinPlot(ax[7], factors1[2], 8, 9)
+    
     # Add subplot labels
     subplotLabel(ax)
-
     return f
