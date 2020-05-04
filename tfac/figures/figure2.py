31b32bbf9e0da795a536529c2ee15002a83894c0
Your Name <shashank18@ucla.edu>
2020-05-04 00:23:28 -0700

    Box Plots

--- tfac/figures/figure2.py
+++ tfac/figures/figure2.py
@@ -2,24 +2,43 @@
 This creates Figure 2.
 """
 import seaborn as sns
+from tensorly.decomposition import parafac
 from .figureCommon import subplotLabel, getSetup
+from .figure1 import treatmentPlot, timePlot, proteinPlot, setPlotLimits
+from ..Data_Mod import form_tensor
 from ..regression import KFoldCV
+from ..tensor import find_R2X_parafac
 
-sns.set(style="white")
-sns.set_context('notebook')
-
+tensor, treatments, times = form_tensor()
+_, factors = parafac(tensor, 12, orthogonalise=True)
 
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
     ax, f = getSetup((7, 6), (2, 2))
 
-    ax[0].axis('off')  # blank out first axis for cartoon
+    R2X_figure(ax[0])
+    treatmentPlot(ax[1], factors[0], 1, 2, treatments)
+    timePlot(ax[2], factors[1], 1, 2, times)
+    proteinPlot(ax[3], factors[2], 1, 2)
 
     # Add subplot labels
     subplotLabel(ax)
     return f
 
+def R2X_figure(ax):
+    '''Create Parafac R2X Figure'''
+    R2X = []
+    nComps = range(1, 14)
+    for i in nComps:
+        R2X.append(find_R2X_parafac(form_tensor()[0], i))
+    ax = sns.scatterplot(nComps, R2X, ax=ax)
+    ax.set_xlabel("Rank Decomposition")
+    ax.set_ylabel("R2X")
+    ax.set_title("CP Decomposition")
+
+
+#### FROM ORIGINAL PROJECT #################################################################################
 
 def predVsActual(ax, x, y, reg):
     '''Predicted vs Actual plotting function for regression'''
--- tfac/tensor.py
+++ tfac/tensor.py
@@ -3,7 +3,7 @@ Tensor decomposition methods
 """
 import numpy as np
 import tensorly as tl
-from tensorly.decomposition import non_negative_parafac, non_negative_tucker
+from tensorly.decomposition import non_negative_parafac, non_negative_tucker, parafac, tucker
 from tensorly.metrics.regression import variance as tl_var
 
 tl.set_backend("numpy")  # Set the backend
@@ -25,24 +25,61 @@ def R2X(reconstructed, original):
     return 1.0 - tl_var(reconstructed - original) / tl_var(original)
 
 
-def perform_decomposition(tensor, r, weightFactor=2):
-    """ Perform PARAFAC decomposition. """
-    weights, factors = non_negative_parafac(tensor, r, tol=1.0e-10, n_iter_max=6000, normalize_factors=True)
-    factors[weightFactor] *= weights[np.newaxis, :]  # Put weighting in designated factor
+def reorient_factors(factors):
+    """ Reorient factors based on the sign of the mean so that only the last factor can have negative means. """
+    for index in range(len(factors) - 1):
+        meann = np.sign(np.mean(factors[index], axis=0))
+        assert meann.size == factors[0].shape[1]
+
+        factors[index] *= meann
+        factors[index + 1] *= meann
+
     return factors
 
 
-def perform_tucker(tensor, rank_list):
-    """ Perform Tucker decomposition. """
-    # index 0 is for core tensor, index 1 is for factors; out is a list of core and factors
-    return non_negative_tucker(tensor, rank_list, tol=1.0e-10, n_iter_max=2000)
+#### Decomposition Methods ###################################################################
 
 
-def find_R2X_tucker(values, out):
+def cp_decomp(tensor, r):
+    """Perform PARAFAC decomposition.
+    -----------------------------------------------
+    Input
+        tensor: 3D data tensor
+        r: rank of decomposition
+    Returns
+        factors: list of factor matrices
+    """
+    weights, factors = parafac(tensor, r, tol=1.0e-10, n_iter_max=6000, orthogonalise=True, normalize_factors=True)
+    factors[2] *= weights[np.newaxis, :]  # Put weighting in designated factor
+    weights /= weights
+    return weights, factors
+
+
+def tucker_decomp(tensor, rank_list, nneg=False):
+    """Perform Tucker decomposition.
+    -----------------------------------------------
+    Input:
+        tensor: 3D data tensor
+        r: rank of decomposition (list of ranks)
+    Returns
+        output[0]: core tensor
+        output[1]: list of factor matrices
+    """
+    if nneg:
+        output = non_negative_tucker(tensor, rank_list, tol=1.0e-10, n_iter_max=2000)
+    else:
+        output = tucker(tensor, rank_list, tol=1.0e-10, n_iter_max=2000)
+    return output
+
+
+#### For R2X Plots ###########################################################################
+
+
+def find_R2X_parafac(cp_output, orig):
     """Compute R2X for the tucker decomposition."""
-    return R2X(tl.tucker_to_tensor(out), values)
+    return R2X(tl.kruskal_to_tensor(cp_output), orig)
 
 
-def find_R2X(values, factors):
-    """Compute R2X from parafac. Note that the inputs values and factors are in numpy."""
-    return R2X(tl.kruskal_to_tensor((np.ones(factors[0].shape[1]), factors)), values)
+def find_R2X_tucker(tucker_output, orig):
+    """Compute R2X for the tucker decomposition."""
+    return R2X(tl.tucker_to_tensor(tucker_output), orig)
