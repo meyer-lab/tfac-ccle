d593084b754d0de0acc1be8a98b11009922cd435
Aditya Sivakumar <asivakumar11@g.ucla.edu>
2020-05-04 22:09:49 -0700

    fix

--- tfac/figures/figure3.py
+++ tfac/figures/figure3.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from .figureCommon import subplotLabel, getSetup
 from .figure1 import treatmentPlot, timePlot, proteinPlot
 from ..Data_Mod import form_tensor
-from ..tensor import cp_decomp, find_R2X_tucker
+from ..tensor import cp_decomp, reorient_factors
 
 tensor, treatments, times = form_tensor()
 results = cp_decomp(tensor, 8)
@@ -17,6 +17,7 @@ def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
     ax, f = getSetup((12, 7), (2, 4))
+    
     for i in range (1,8):
         treatmentPlot(ax[i-1], comps[0], i,i+1, treatments)
 
