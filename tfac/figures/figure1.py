82cc3c33905c4e924ce2995726decb2fb76d1aff
Scott T <sco.d.taylor@gmail.com>
2020-05-21 20:57:41 -0700

    Move plots to figure files

--- tfac/figures/figure1.py
+++ tfac/figures/figure1.py
@@ -1,92 +1,47 @@
 """
-This creates Figure 1 - CP Decomposition Plots
+This creates Figure 1 - Partial Tucker Decomposition Protein Plots
 """
 import numpy as np
 import seaborn as sns
 import pandas as pd
 from .figureCommon import subplotLabel, getSetup
-from ..tensor import cp_decomp, find_R2X_parafac, reorient_factors
-from ..Data_Mod import form_tensor, z_score_tensor_bymeasure, z_score_tensor_byprotein
+from ..tensor import partial_tucker_decomp
+from ..Data_Mod import form_tensor
 
+components = 7
 tensor, treatments, times = form_tensor()
-tensor_z = z_score_tensor_byprotein(tensor)
-results = cp_decomp(tensor_z, 8)
-comps = reorient_factors(results[1])
+results = partial_tucker_decomp(tensor, [2], components)
 
 
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
-    ax, f = getSetup((12, 12), (2, 2))
+    row = 3
+    col = 7
+    ax, f = getSetup((21, 7), (row, col))
 
-    R2X_figure(ax[0], tensor_z)
-    treatmentPlot(ax[1], comps[0], treatments)
-    timePlot(ax[2], comps[1], times)
-    proteinBoxPlot(ax[3], comps[2])
-
+    proteinScatterPlot(ax, results, components)
+    
     # Add subplot labels
     subplotLabel(ax)
 
     return f
 
 
-def R2X_figure(ax, input_tensor):
-    '''Create Parafac R2X Figure'''
-    R2X = np.zeros(13)
-    nComps = range(1, len(R2X))
-    for i in nComps:
-        output = cp_decomp(input_tensor, i)
-        R2X[i] = find_R2X_parafac(output, input_tensor)
-    sns.scatterplot(np.arange(len(R2X)), R2X, ax=ax)
-    ax.set_xlabel("Rank Decomposition")
-    ax.set_ylabel("R2X")
-    ax.set_title("CP Decomposition")
-    ax.set_yticks([0, .2, .4, .6, .8, 1])
-
-
-def treatmentPlot(ax, factors, senthue):
-    '''Plot treatments (tensor axis 0) in factorization component space'''
-    df = pd.DataFrame(factors).T
-    markers = ['o', 's', 'X', 'h', '^', 'D', 'P']
-    df.columns = senthue
-    df["Components"] = range(1, factors.shape[1] + 1)
-    df = df.set_index('Components')
-    sns.scatterplot(data=df, ax=ax, markers=markers, palette='bright', s=100)
-    ax.set_xlabel('Component')
-    ax.set_ylabel('Component Value')
-    ax.set_title('Treatment Factors')  
-
-
-def timePlot(ax, factors, senthue):
-    '''Plot time points (tensor axis 1) in factorization component space'''
-    df = pd.DataFrame(factors)
-    columns = []
-    for i in range(1, (factors.shape[1] + 1)):
-        columns.append('Component ' + str(i))
-    df.columns = columns
-    df['Measurement Time'] = senthue
-    df = df.set_index('Measurement Time')
-    sns.lineplot(data=df, ax=ax, palette='tab20', dashes=False)
-    ax.set_xlabel("Measurement Time")
-    ax.set_ylabel('Component Value')
-    ax.set_title('Time Factors')
-
-def proteinBoxPlot(ax, factors):
-    '''Plot proteins (tensor axis 2) in factorization component space'''
-    df = pd.DataFrame(factors)
-    complist = range(1, (factors.shape[1] + 1))
-    df.columns = complist
-    sns.boxplot(data = df, ax = ax)
-    ax.set_xlabel("Component")
-    ax.set_ylabel('Component Value')
-    ax.set_title('Protein Factors')
-
-def proteinScatterPlot(ax, factors, r1, r2):
+def proteinScatterPlot(ax, results, components):
     '''Plot compared proteins (tensor axis 2) in factorization component space'''
-    sns.scatterplot(factors[:, r1 - 1], factors[:, r2 - 1], ax=ax)
-    ax.set_xlabel('Component ' + str(r1))
-    ax.set_ylabel('Component ' + str(r2))
-    ax.set_title('Protein Factors')
+    counter = 0
+    for i in range(components):
+        for j in range(i + 1, components):
+            sns.scatterplot(results[1][0][:, i], results[1][0][:, j], ax=ax[counter])
+            ax[counter].set_xlabel('Component ' + str(i + 1))
+            ax[counter].set_ylabel('Component ' + str(j + 1))
+            ax[counter].set_title('Protein Factors')
+            counter += 1
+    for _ in range(counter, len(ax)):
+        ax[counter].axis('off')
+        counter += 1
+    
 
 def setPlotLimits(axis, factors, r1, r2):
     '''Set appropriate limits for the borders of each component plot'''
@@ -96,23 +51,3 @@ def setPlotLimits(axis, factors, r1, r2):
     ylim = 1.1 * np.max(y)
     axis.set_xlim((-xlim, xlim))
     axis.set_ylim((-ylim, ylim))
-
-def separate_treatmentPlot(factors, senthue):
-    '''Plot treatments (tensor axis 0) in factorization component space'''
-    df = pd.DataFrame(factors)
-    df.columns = range(1, factors.shape[1] + 1)
-    df["Treatment"] = senthue
-    df = pd.melt(df, "Treatment")
-    df.columns = ["Treatment", "Component", "Value"]
-    grid = sns.FacetGrid(df, col="Treatment", hue="Treatment", col_wrap=4, height=4, sharex=False)
-    grid.map(plt.plot, "Component", "Value")
-    for i in range(1, factors.shape[1] + 1):
-        grid.map(plt.axvline, x=i, ls=":", c=".7")
-    for i in np.arange(-.8, 1, .2):
-        grid.map(plt.axhline, y=i, ls=":", c=".7")
-    grid.map(plt.axhline, y=0, ls="-", c=".5")
-    grid.set(xticks=np.arange(1, factors.shape[1] + 1))
-    grid.set(yticks=np.arange(-.8, 1, .2))
-    for i in range(factors.shape[0]):
-        grid.axes[i].set_xlabel('Components')
-    grid.fig.tight_layout(w_pad=1)
--- tfac/figures/figure2.py
+++ tfac/figures/figure2.py
@@ -1,45 +1,47 @@
 """
-This creates Figure 2.
+This creates Figure 2 - Partial Tucker Decomposition Treatment and Time Plots.
 """
 import seaborn as sns
-from tensorly.decomposition import parafac
+import pandas as pd
 from .figureCommon import subplotLabel, getSetup
-from .figure1 import treatmentPlot, timePlot, proteinBoxPlot, setPlotLimits
+from ..tensor import partial_tucker_decomp
 from ..Data_Mod import form_tensor
-from ..regression import KFoldCV
-from ..tensor import find_R2X_parafac
 
+components = 7
 tensor, treatments, times = form_tensor()
-_, factors = parafac(tensor, 12, orthogonalise=True)
+results = partial_tucker_decomp(tensor, [2], components)
+
+
 
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
-    ax, f = getSetup((7, 6), (2, 2))
+    row = 2
+    col = 4
+    ax, f = getSetup((7, 6), (row, col))
+    
+    treatmentvsTimePlot(results, components, ax)
 
     # Add subplot labels
     subplotLabel(ax)
     return f
 
-def R2X_figure(ax):
-    '''Create Parafac R2X Figure'''
-    R2X = []
-    nComps = range(1, 14)
-    for i in nComps:
-        R2X.append(find_R2X_parafac(form_tensor()[0], i))
-    ax = sns.scatterplot(nComps, R2X, ax=ax)
-    ax.set_xlabel("Rank Decomposition")
-    ax.set_ylabel("R2X")
-    ax.set_title("CP Decomposition")
 
+def treatmentvsTimePlot(results, components, ax):
+    frame_list = []
+    for i in range(7):
+        df = pd.DataFrame(results[0][i])
+        frame_list.append(df)
 
-#### FROM ORIGINAL PROJECT #################################################################################
-
-def predVsActual(ax, x, y, reg):
-    '''Predicted vs Actual plotting function for regression'''
-    _, predicted, actual = KFoldCV(x, y, reg)
-    sns.scatterplot(actual, predicted, color='darkslategrey', ax=ax)
-    sns.despine()
-    ax.set_xlabel('Actual')
-    ax.set_ylabel('Predicted')
-    ax.set_title('Predicted vs Actual ' + reg)
+    for component in range(7):
+        column_list = []
+        for i in range(7):
+            column_list.append(pd.DataFrame(frame_list[i].iloc[:, component]))
+        df = pd.concat(column_list, axis=1)
+        df.columns = treatments
+        df['Times'] = [0, 2, 4, 8, 24, 48]
+        df = df.set_index('Times')
+        b = sns.lineplot(data=df, ax=ax[component], dashes=None)
+        b.set_title('Component ' + str(component+1))
+    for i in range(component + 1, len(ax)):
+        ax[i].axis('off')
