2cc9302e07bc4ce587b44e9f067fc81038b18f10
Scott T <sco.d.taylor@gmail.com>
2020-05-27 18:05:46 -0700

    R2X fix, parafac2 working

--- tfac/figures/figure5.py
+++ tfac/figures/figure5.py
@@ -1,17 +1,38 @@
 """
 This creates Figure 5.
 """
+import numpy as np
 from .figureCommon import subplotLabel, getSetup
+from ..dataHelpers import form_MRSA_tensor
+from tensorly.decomposition import parafac2
+import tensorly as tl
+from tensorly.parafac2_tensor import parafac2_to_slice
+from tensorly.metrics.regression import variance as tl_var
+
+
+tl.set_backend("numpy")
 
 
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
-    ax, f = getSetup((7.5, 6), (3, 4))
+    
+    tensor_slices, cytokines, geneIDs = form_MRSA_tensor()
+    
+    print(tl_var(tensor_slices[1])/tl_var(tensor_slices[0]))
+    
+    AllR2X = []
+    for i in range(1, 11):
+        parafac2tensor = parafac2(tensor_slices, i, random_state=1)
+        AllR2X.append(R2Xparafac2(tensor_slices, parafac2tensor))
+    
+    print(AllR2X)
 
-    ax[0].axis('off')  # blank out first axis for cartoon
+    return tensor_slices
 
-    # Add subplot labels
-    subplotLabel(ax)
-
-    return f
+def R2Xparafac2(tensor_slices, decomposition):
+    R2X = [0, 0]
+    for idx, tensor_slice in enumerate(tensor_slices):
+        reconstruction = parafac2_to_slice(decomposition, idx, validate=False)
+        R2X[idx] = 1.0 - tl_var(reconstruction - tensor_slice) / tl_var(tensor_slice)
+    return R2X
