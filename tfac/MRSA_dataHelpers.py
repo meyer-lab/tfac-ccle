8a9a3028d5c437a2d961e540c8e7cd5605f1fa35
scottdtaylor95 <48601443+scottdtaylor95@users.noreply.github.com>
2020-06-11 21:14:07 -0700

    Update MRSA_dataHelpers.py

--- tfac/MRSA_dataHelpers.py
+++ tfac/MRSA_dataHelpers.py
@@ -16,7 +16,7 @@ def get_patient_info():
     return cohortID, statusID
 
 
-def form_MRSA_tensor():
+def form_MRSA_tensor(variance):
     """Create list of data matrices for parafac2"""
     dfClin, dfCoh = importClinicalMRSA()
     dfCyto = clinicalCyto(dfClin, dfCoh)
@@ -34,8 +34,7 @@ def form_MRSA_tensor():
     expNumpy = dfExp.to_numpy().T
 
     expNumpy = expNumpy.astype(float)
-    var = tl_var(expNumpy) / tl_var(cytoNumpy)
-    cytoNumpy = cytoNumpy * 29
+    cytoNumpy = cytoNumpy * variance
 
     tensor_slices = [cytoNumpy, expNumpy]
 
