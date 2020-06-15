716fb00e17289e89e1942311934885d3607af90d
scottdtaylor95 <48601443+scottdtaylor95@users.noreply.github.com>
2020-06-15 15:20:16 -0700

    Update figure3.py

--- tfac/figures/figure3.py
+++ tfac/figures/figure3.py
@@ -9,42 +9,25 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold
 from sklearn.metrics import roc_auc_score
 from .figureCommon import subplotLabel, getSetup
-from ..MRSA_dataHelpers import form_MRSA_tensor, get_patient_info
+from ..MRSA_dataHelpers import form_MRSA_tensor, get_patient_info, find_CV_AUC, produce_outcome_bools
 
-cohortID, outcomeID = get_patient_info()
-
-outcome_bools = []
-
-for outcome in outcomeID:
-    if outcome == 'APMB':
-        outcome_bools.append(0)
-    else:
-        outcome_bools.append(1)
-
-outcomes = np.asarray(outcome_bools)
-true_y = outcomes
+_, outcomeID = get_patient_info()
+true_y = produce_outcome_bools(outcomeID)
 
 values_comps = []
 for components in range(1, 39):
     tensor_slices, cytokines, geneIDs = form_MRSA_tensor(1)
     parafac2tensor = None
     best_error = np.inf
-    for run in range(4):
-        decomposition, errors = parafac2(tensor_slices, components, return_errors=True)
+    for run in range(1):
+        decomposition, errors = parafac2(tensor_slices, components, return_errors=True, n_iter_max=1000)
         if best_error > errors[-1]:
             best_error = errors[-1]
             parafac2tensor = decomposition
 
     patient_matrix = parafac2tensor[1][2]
-
-
-    kf = KFold(n_splits=61)
-    decisions = []
-    for train, test in kf.split(patient_matrix):
-        clf = LogisticRegression(random_state=1, max_iter=10000).fit(patient_matrix[train], outcomes[train])
-        decisions.append(clf.decision_function(patient_matrix[test]))
-    score_y = decisions
-    auc = roc_auc_score(true_y, score_y)
+    
+    auc = find_CV_AUC(patient_matrix, true_y)
     values_comps.append([components, auc])
 df_comp = pd.DataFrame(values_comps)
 df_comp.columns = ['Components', 'AUC']
