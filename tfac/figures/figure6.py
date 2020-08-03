d368a1632cff6d76c4ea59f5980fb07ebf0fb6a5
Your Name <you@example.com>
2020-08-02 23:42:04 -0700

    combining plots

--- tfac/figures/figure6.py
+++ tfac/figures/figure6.py
@@ -1,12 +1,14 @@
 """
-This creates Figure 6 - ROC Curve.
+This creates Figure 4.
 """
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import numpy as np
-from .figureCommon import getSetup #, subplotLabel
+from .figureCommon import subplotLabel, getSetup
 from ..Data_Mod import form_parafac2_tensor, ohsu_var
-from ..tensor import OHSU_parafac2_decomp, projections_to_factors
+from ..tensor import OHSU_parafac2_decomp, R2Xparafac2, projections_to_factors
+
 
 p2slices, treatmentsTime, proteins, chromosomes, IFproteins, histones, geneExpression, RNAGenes, Rproteins = form_parafac2_tensor()
 p2slicesB = ohsu_var(p2slices)
@@ -14,52 +16,70 @@ components = 5
 parafac2tensor, error = OHSU_parafac2_decomp(p2slicesB, components)
 weights, transform = projections_to_factors(parafac2tensor)
 LINCSproteins = transform[1][0]
-atacChr = transform[1][1]
+#atacChr = transform[1][1]
 GCPHistones = transform[1][3]
 L1000GeneExp = transform[1][4]
-RNAGeneSeq = transform[1][5]
+#RNAGeneSeq = transform[1][5]
 RPPAproteins = transform[1][6]
+C = parafac2tensor[1][2]
+df = pd.DataFrame(C[:-1, :])
+df.columns = ["1", "2", "3", "4", "5"]
+treatments = ['BMP2', 'BMP2', 'EGF', 'EGF', 'HGF', 'HGF', 'IFNg', 'IFNg', 'OSM', 'OSM', 'PBS', 'PBS', 'TGFb', 'TGFb', 'BMP2', 'EGF', 'HGF', 'IFNg', 'OSM', 'PBS', 'TGFb']
+times = [24, 48] * 7 + [0] * 7
+zeros = pd.DataFrame(C[-1:, :])
+zeros.columns = ["1", "2", "3", "4", "5"]
+for _ in range(7):
+    df = pd.concat((df, zeros))
+df["Times"] = times
+df["Treatments"] = treatments
 
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
-    row = 4
+    row = 6
     col = 5
     ax, f = getSetup((40, 40), (row, col))
-    proteinBoxPlot(ax[0], LINCSproteins[:,0], 1, proteins)
-    proteinBoxPlot(ax[1], LINCSproteins[:,1], 2, proteins)
-    proteinBoxPlot(ax[2], LINCSproteins[:,2], 3, proteins)
-    proteinBoxPlot(ax[3], LINCSproteins[:,3], 4, proteins)
-    proteinBoxPlot(ax[4], LINCSproteins[:,4], 5, proteins)
-    proteinBoxPlot(ax[5], GCPHistones[:,0], 1, histones)
-    proteinBoxPlot(ax[6], GCPHistones[:,1], 2, histones)
-    proteinBoxPlot(ax[7], GCPHistones[:,2], 3, histones)
-    proteinBoxPlot(ax[8], GCPHistones[:,3], 4, histones)
-    proteinBoxPlot(ax[9], GCPHistones[:,4], 5, histones)
-    proteinBoxPlot(ax[10], L1000GeneExp[:,0], 1, geneExpression)
-    proteinBoxPlot(ax[11], L1000GeneExp[:,1], 2, geneExpression)
-    proteinBoxPlot(ax[12], L1000GeneExp[:,2], 3, geneExpression)
-    proteinBoxPlot(ax[13], L1000GeneExp[:,3], 4, geneExpression)
-    proteinBoxPlot(ax[14], L1000GeneExp[:,4], 5, geneExpression)
-    proteinBoxPlot(ax[15], RPPAproteins[:,0], 1, Rproteins)
-    proteinBoxPlot(ax[16], RPPAproteins[:,1], 2, Rproteins)
-    proteinBoxPlot(ax[17], RPPAproteins[:,2], 3, Rproteins)
-    proteinBoxPlot(ax[18], RPPAproteins[:,3], 4, Rproteins)
-    proteinBoxPlot(ax[19], RPPAproteins[:,4], 5, Rproteins)
-    # Add subplot labels
-   # subplotLabel(ax)
-
+    R2X_OHSU(ax[0])
+    for comp in range(1, 6):
+        OHSU_comp_plots(df, comp, ax[comp + 4])
+    for comps in range(5):
+        proteinBoxPlot(ax[10 + comps], LINCSproteins[:, comps], comps + 1, proteins)
+        proteinBoxPlot(ax[15 + comps], GCPHistones[:, comps], comps + 1, histones)
+        proteinBoxPlot(ax[20 + comps], L1000GeneExp[:, comps], comps + 1, geneExpression)
+        proteinBoxPlot(ax[25 + comps], RPPAproteins[:, comps], comps +1, Rproteins)
+    subplotLabel(ax)
     return f
 
 
-#R2X
-#comp plots
-#def transformPlot(ax, seed, transformSlice, sliceRow):
-#    '''Plots box plot along each component for each slice measurement label after projection to factor transformation'''
-#    col = np.size(transformSlice, 1)
-#    for a in range(col):
-#        proteinBoxPlot(ax[seed + a], transformSlice[:,a], a+1, sliceRow)
-    
+def R2X_OHSU(ax):
+    '''Creates R2X for OHSU PARAFAC2'''
+    p2slices, treatmentsTime, proteins, chromosomes, IFproteins, histones, geneExpression, RNAGenes, RPPAProteins = form_parafac2_tensor()
+    p2slicesB = ohsu_var(p2slices)
+    r2x = []
+    components = 10
+    for i in range(1, components + 1):
+        parafac2tensor, error = OHSU_parafac2_decomp(p2slicesB, i)
+        r2x.append(R2Xparafac2(p2slicesB, parafac2tensor))
+    df = pd.DataFrame(r2x)
+    comps = []
+    for i in range(1, components + 1):
+        comps.append(i)
+    df['Component'] = comps
+    df.columns = ['Proteins', 'Chromosomes', 'IFproteins', 'Histones', 'Gene Expression', 'RNA Genes', 'RPPA Proteins', 'Component']
+    test = pd.melt(df, id_vars=['Component'])
+    b = sns.scatterplot(data=test, x='Component', y='value', hue='variable', style='variable', ax=ax, s=100)
+    b.set_xlabel("Component", fontsize=20)
+    b.set_ylabel("R2X", fontsize=20)
+    b.set_title("OHSU PARAFAC2")
+    b.tick_params(labelsize=15)
+    plt.legend(prop={'size': 15})
+    ax.set_ylim(0, 1)
+
+def OHSU_comp_plots(df, comps, ax):
+    sns.lineplot(data=df, x="Times", y=str(comps), hue="Treatments", ax=ax)
+    ax.set_xlabel('Time (hr)')
+    ax.set_title('Component ' + str(comps))
+
 def proteinBoxPlot(ax, transformed, componentIn, sliceRows):
     '''Plots protein component in partial tucker factorization space with annotation of some outliers'''
     df = pd.DataFrame(transformed)
@@ -111,4 +131,4 @@ def outliersForPlot(transformed, sliceRows):
                     random2 = np.random.randint(0, 2)
                     prots[i][idx + random2 * 4][3] = False
                     prots[i][idx + 4][4] = False
-    return prots
+    return prots    
