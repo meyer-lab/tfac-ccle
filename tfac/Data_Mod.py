cea940070ba7a6d3ef54452f1a6f42b8297968b8
Your Name <you@example.com>
2020-08-24 20:33:25 -0700

    debugging figure reorg

--- tfac/Data_Mod.py
+++ tfac/Data_Mod.py
@@ -1,8 +1,11 @@
 """Data pre-processing and tensor formation"""
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from tensorly.metrics.regression import variance as tl_var
 from .dataHelpers import importLINCSprotein, ohsu_data
+from .tensor import OHSU_parafac2_decomp, R2Xparafac2
 
 
 def data_mod(x, df=None):
@@ -119,3 +122,86 @@ def ohsu_var(tensorSlices):
     tensorSlices[0] = tensorSlices[0] * 12
     tensorSlices[6] = tensorSlices[6] * 12
     return tensorSlices
+
+def R2X_OHSU(ax, p2slicesB):
+    '''Creates R2X for OHSU PARAFAC2'''
+    r2x = []
+    compR2X = 10
+    for i in range(1, compR2X + 1):
+        parafac2tensor, error = OHSU_parafac2_decomp(p2slicesB, i)
+        r2x.append(R2Xparafac2(p2slicesB, parafac2tensor))
+    df = pd.DataFrame(r2x)
+    comps = []
+    for i in range(1, compR2X + 1):
+        comps.append(i)
+    df['Component'] = comps
+    df.columns = ['Proteins', 'Chromosomes', 'IFproteins', 'Histones', 'Gene Expression', 'RNA Genes', 'RPPA Proteins', 'Component']
+    test = pd.melt(df, id_vars=['Component'])
+    b = sns.scatterplot(data=test, x='Component', y='value', hue='variable', style='variable', ax=ax, s=100)
+    b.set_xlabel("Component", fontsize=20)
+    b.set_ylabel("R2X", fontsize=20)
+    b.set_title("OHSU PARAFAC2")
+    b.tick_params(labelsize=15)
+    plt.legend(prop={'size': 15})
+    ax.set_ylim(0, 1)
+
+
+def OHSU_comp_plots(df, comps, ax):
+    '''Plots treatments by each component from PARAFAC2'''
+    sns.lineplot(data=df, x="Times", y=str(comps), hue="Treatments", ax=ax)
+    ax.set_xlabel('Time (hr)')
+    ax.set_title('Component ' + str(comps))
+
+
+def proteinBoxPlot(ax, transformed, componentIn, sliceRows):
+    '''Plots protein component in partial tucker factorization space with annotation of some outliers'''
+    df = pd.DataFrame(transformed)
+    prots = outliersForPlot(df, sliceRows)
+    sns.boxplot(data=df, ax=ax)
+    ax.set_xlabel("Component " + str(componentIn))
+    ax.set_ylabel('Component Value')
+    ax.set_title('Protein Factors')
+    for component in prots:
+        offset_side = 0
+        for outlier in prots[component]:
+            if outlier[3]:
+                if offset_side == 0:
+                    ax.text(outlier[0] + .05, outlier[1] - .005, outlier[2], horizontalalignment='left', size='large', color='black', weight=100)
+                    offset_side = 1
+                elif offset_side == 1:
+                    ax.text(outlier[0] - .05, outlier[1] - .005, outlier[2], horizontalalignment='right', size='large', color='black', weight=100)
+                    offset_side = 0
+            else:
+                offset_side = 1 - offset_side
+
+
+def outliersForPlot(transformed, sliceRows):
+    '''Determines outliers based on IQR range by component and returns dictionary by component that allows annotation for OHSU PARAFAC2 analysis'''
+    df = transformed.copy(deep=True)
+    df['Proteins'] = sliceRows
+    Q1 = df.quantile(.25)
+    Q3 = df.quantile(.75)
+    IQR = Q3 - Q1
+    prots = {}
+    for i in range(df.columns.size - 1):
+        prots[i] = []
+        for _, col in df.iterrows():
+            if (col[i] < (Q1[i] - 1.7 * IQR[i])) or (col[i] > (Q3[i] + 1.7 * IQR[i])):
+                tup = [i, col[i], col['Proteins'][:-1], True, True]
+                prots[i].append(tup)
+        prots[i].sort(key=lambda x: x[1])
+        for idx, tup in enumerate(prots[i]):
+            if idx < len(prots[i]) - 4:
+                if tup[1] > prots[i][idx + 2][1] - .012 and tup[3] == tup[4]:
+                    random1 = np.random.choice([0, 1, 1])
+                    prots[i][idx + (random1 * 2)][3] = False
+                    tup[4] = False
+                    prots[i][idx + 2][4] = False
+                elif tup[1] > prots[i][idx + 2][1] - .012 and tup[3]:
+                    prots[i][idx + 2][3] = False
+                    prots[i][idx + 2][4] = False
+                if tup[1] > prots[i][idx + 4][1] - .012 and tup[3]:
+                    random2 = np.random.randint(0, 2)
+                    prots[i][idx + random2 * 4][3] = False
+                    prots[i][idx + 4][4] = False
+    return prots
