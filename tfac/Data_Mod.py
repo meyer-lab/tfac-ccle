506a98063887b734f708614822b293a41655e717
scottdtaylor95 <48601443+scottdtaylor95@users.noreply.github.com>
2020-09-08 19:10:29 -0700

    Update Data_Mod.py

--- tfac/Data_Mod.py
+++ tfac/Data_Mod.py
@@ -8,48 +8,6 @@ from .dataHelpers import ohsu_data
 from .tensor import OHSU_parafac2_decomp, R2Xparafac2
 
 
-def data_mod(x, df=None):
-    """Creates a slice of the data tensor corresponding to the inputted treatment"""
-    '''
-    if not isinstance(df, pd.core.frame.DataFrame):
-        df = importLINCSprotein()
-    spec_df = df.loc[(df["Treatment"] == "Control") | (df["Treatment"] == x)]
-    times = spec_df["Time"].to_numpy().tolist()
-    spec_df = spec_df.drop(columns=["Sample description", "Treatment", "Time"])
-    y = spec_df.to_numpy()
-    return y, spec_df, times
-    '''
-
-
-def form_tensor():
-    """Creates tensor in numpy array form and returns tensor, treatments, and time"""
-'''
-    df = importLINCSprotein()
-    tempindex = df["Sample description"]
-    tempindex = tempindex[:36]
-    i = 0
-    for a in tempindex:
-        tempindex[i] = a[3:]
-        i += 1
-    treatments = df["Treatment"][0:36]
-    times = df["Time"][0:36]
-    df = df.drop(["Sample description"], axis=1)
-    by_row_index = df.groupby(df.index)
-    df_means = by_row_index.mean()
-    df_means.insert(0, "Treatment", value=treatments)
-    df_means.insert(0, "Sample description", tempindex)
-    unique_treatments = np.unique(df_means["Treatment"].values).tolist()
-    unique_treatments.remove("Control")
-
-    slices = []
-    for treatment in unique_treatments:
-        array, _, times = data_mod(treatment, df_means)
-        slices.append(array)
-    tensor = np.stack(slices)
-    return tensor, unique_treatments, times
-'''
-
-
 def dataCleanUp():
     """Cleaning up OHSU data for PARAFAC2 column order"""
     atac, cycIF, GCP, _, L1000, RNAseq, RPPA = ohsu_data()
