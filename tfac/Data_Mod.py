277f48ffa851ee07074548d74bb24176fc609c6b
Your Name <you@example.com>
2020-11-22 23:53:34 -0800

    adding back parafac2 code for gsea

--- tfac/Data_Mod.py
+++ tfac/Data_Mod.py
@@ -1,7 +1,8 @@
 """Data pre-processing and tensor formation"""
 import pandas as pd
 import numpy as np
-from .dataHelpers import importLINCSprotein
+from tensorly.metrics.regression import variance as tl_var
+from dataHelpers import importLINCSprotein, dataCleanUp
 
 
 def data_mod(x, df=None):
@@ -40,3 +41,30 @@ def form_tensor():
         slices.append(array)
     tensor = np.stack(slices)
     return tensor, unique_treatments, times
+
+def form_parafac2_tensor():
+    """Creates tensor in numpy form and returns tensor, treatment by time, LINCS proteins, ATAC chromosomes, IF proteins, GCP histones, L1000 gene expression, RNA gene sequence, and RPPA proteins"""
+    atacM, cycIFM, GCPM, L1000M, RNAseqM, RPPAM, chromosomes, IFproteins, histones, geneExpression, RNAGenes, RPPAProteins = dataCleanUp()
+    p2slices = [atacM, cycIFM, GCPM, L1000M, RNAseqM, RPPAM]
+    for x in range(len(p2slices)):
+        df = pd.DataFrame(p2slices[x], columns=['BMP2_24', 'BMP2_48', 'EGF_24', 'EGF_48', 'HGF_24', 'HGF_48', 'IFNG_24', 'IFNG_48', 'OSM_24', 'OSM_48', 'TGFB_24', 'TGFB_48', 'PBS_24', 'PBS_48', 'ctrl_0'])
+        df = df.replace('5.!21302089907794', 5.21302089907794)
+        df = df.replace('6>029875019867487', 6.029875019867487)
+        df = df.replace('6>035425175013243', 6.035425175013243)
+        df = df.replace('6.66!312194765656', 6.66312194765656).astype(float)
+        df = df - (df.apply(np.mean)) / df.apply(np.std)
+        df = (df.sub(df.apply(np.mean, axis=1).to_list(), axis=0)).div(df.apply(np.std, axis=1).to_list(), axis=0)
+        p2slices[x] = df.to_numpy()
+    return p2slices, chromosomes, IFproteins, histones, geneExpression, RNAGenes, RPPAProteins
+
+def ohsu_var(tensorSlices):
+    '''Rebalances variance of all tensor slices to 1'''
+    for x, val in enumerate(tensorSlices):
+        var = tl_var(tensorSlices[x])
+        tensorSlices[x] = (tensorSlices[x]) / (var ** 0.5)
+    #tensorSlices[1] = tensorSlices[1] * 3
+    tensorSlices[2] = tensorSlices[2] * 20
+    #tensorSlices[3] = tensorSlices[3] * 12
+    tensorSlices[5] = tensorSlices[5] * 15
+    return tensorSlices
+
