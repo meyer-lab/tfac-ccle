9a93a91fcdc87f3ab6c96ca84b6cc23076cff3d6
Aditya Sivakumar <asivakumar11@g.ucla.edu>
2021-02-28 17:03:01 -0800

    fixed imports and other issues

--- tfac/figures/figure1.py
+++ tfac/figures/figure1.py
@@ -7,6 +7,7 @@ import seaborn as sns
 from .figureCommon import subplotLabel, getSetup
 from ..tensor import partial_tucker_decomp, find_R2X_partialtucker, flip_factors
 from ..Data_Mod import form_tensor
+from ..pseudoinvnorm import get_Flattened_Matrices, get_reconstruct, find_gene_factors
 from ..dataHelpers import importLINCSprotein
 from tensorly.decomposition import  partial_tucker,parafac2
 import tensorly as tl
@@ -49,51 +50,6 @@ def R2X_Figure_PartialTucker(ax, input_tensor):
     ax.set_xlabel("Components")
     ax.set_ylabel("Protein R2X")
     ax.set_yticks([0, .2, .4, .6, .8, 1])
-    
-def get_Flattened_Matrices(result, geneexpression, treatment_list, times):
-    ids = geneexpression["ensembl_gene_id"]
-    geneexpression.drop("ensembl_gene_id", inplace = True, axis = 1)
-    #create a 5x42 DataFrame of decompsed component values
-    toflatten = result[0]
-    flattened = []
-
-    for treatment in range(7):
-        flattened.append(pd.DataFrame(toflatten[treatment]).T)
-
-    df = pd.concat(flattened, axis=1)
-    
-    #component values for control at T=0
-    ctrl = df.iloc[:, 0]
-
-    #Remove all times that aren't 24 or 48
-    df.drop([0,1,2,3], inplace = True, axis = 1)
-    
-    #add control back in
-    ctrl = pd.DataFrame(ctrl)
-    df = pd.concat([ctrl,df], axis = 1)
-
-    #rename columns
-    cols = [str(x) + "_" + str(y) for x in treatment_list for y in times[-2:]]
-    cols = ["ctrl_0"] + cols
-    df.columns = cols
-
-    #sort by column name
-    df = df.reindex(sorted(df.columns), axis=1)
-    geneexpression = geneexpression.reindex(sorted(geneexpression.columns), axis=1)
-
-    #make dataframes into numpy arrays
-    df = df.to_numpy()
-    genexpression = geneexpression.to_numpy()
-    return df, genexpression
-
-def get_reconstruct(P,X):
-    Ppinv = np.linalg.pinv(P.T)
-    return Ppinv, np.matmul(Ppinv,X.T)
-
-def find_gene_factors(result, geneexpression, treatment_list, times):
-    P, X = get_Flattened_Matrices(result, geneexpression, treatment_list, times)
-    Ppinv, W = get_reconstruct(P, X)
-    return P, X, Ppinv, W
 
 def gene_R2X(axis):
     tensor, treatment_list, times = form_tensor()
