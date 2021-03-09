e4350b392860975a95653db61d119f2774eeb5af
Aaron Meyer <ameyer@ucla.edu>
2021-03-09 13:20:43 -0800

    Last edits

--- tfac/figures/figure2.py
+++ tfac/figures/figure2.py
@@ -19,17 +19,17 @@ def makeFigure():
 
     result = flip_factors(pre_flip_result)
 
-    compList = ['1', '2', '3', '4', '5']
+    compList = ["1", "2", "3", "4", "5"]
     trmtTimeList = []
     for i, name in enumerate(treatment_list):
         for t in range(len(times)):
-            trmtTimeList.append(name + '_' + str(times[t]))
+            trmtTimeList.append(name + "_" + str(times[t]))
     trmtTime = np.reshape(trmtTimeList, (7, 6))
     trmtMap = pd.DataFrame()
     for y in range(len(trmtTime)):
         temp = pd.DataFrame(data=result[0][y], index=trmtTime[y], columns=compList)
         trmtMap = trmtMap.append(temp)
-        palette = sns.cubehelix_palette(len(trmtTime), light=.8, dark=.2, reverse=True, start=1, rot=-2)
+        palette = sns.cubehelix_palette(len(trmtTime), light=0.8, dark=0.2, reverse=True, start=1, rot=-2)
     dict = {}
     time_tick = []
     for x in range(7):
@@ -37,9 +37,9 @@ def makeFigure():
             dict[str(trmtTime[x, y])] = palette[x]
             time_tick.append(times[y])
     colors = pd.Series(trmtTimeList, index=trmtMap.index).map(dict)
-    f = sns.clustermap(trmtMap, row_cluster=False, col_cluster=False, row_colors=colors, cmap='PiYG', center=0, yticklabels=time_tick)
+    f = sns.clustermap(trmtMap, row_cluster=False, col_cluster=False, row_colors=colors, cmap="PiYG", center=0, yticklabels=time_tick)
     for val, label in enumerate(trmtTime[:, 0]):
         f.ax_col_dendrogram.bar(0, 0, color=dict[label], label=treatment_list[val], linewidth=0)
     f.ax_col_dendrogram.legend(loc="center left", ncol=5)
-    f.fig.suptitle('Treatment-Time vs. Component')
+    f.fig.suptitle("Treatment-Time vs. Component")
     return f
