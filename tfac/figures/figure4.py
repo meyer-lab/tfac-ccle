0161aa6f6ca39046c862db28ca66d534dd1b1dba
Your Name <shashank18@ucla.edu>
2021-03-19 22:45:12 -0700

    protein list distribution

--- tfac/figures/figure4.py
+++ tfac/figures/figure4.py
@@ -4,11 +4,13 @@ This creates Figure 4. Gene expression R2X with flattened matrix dimension recon
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from tensorly.metrics.regression import variance as tl_var
 from .figureCommon import subplotLabel, getSetup
 from ..Data_Mod import form_tensor
 from ..tensor import decomp_to_flipped_factors
 from ..pseudoinvnorm import find_factors
+from ..dataHelpers import proteinNames
 
 
 def makeFigure():
@@ -16,24 +18,44 @@ def makeFigure():
     # Get list of axis objects
     row = 1
     col = 1
-    ax, f = getSetup((4, 4), (row, col))
-    gene_R2X(ax[0])
+    ax, f = getSetup((70, 10), (row, col))
+    makeProteinListDistribution(ax[0])
     subplotLabel(ax)
     return f
     
 
-def gene_R2X(axis):
-    RNAseq = pd.read_csv("tfac/data/ohsu/MDD_RNAseq_Level4.csv")
-    RNAseq.drop("ensembl_gene_id", inplace=True, axis=1)
-    R2X = np.zeros(13)
-    for i in range(1, 13):
-        result, treatment_list, times = decomp_to_flipped_factors(i)
-        P, X, _, W = find_factors(result, RNAseq, treatment_list, times)
-        Gene_redone = np.matmul(W.T, P)
-        R2X[i] = 1 - tl_var(Gene_redone - X) / tl_var(RNAseq.to_numpy())
-    sns.scatterplot(np.arange(len(R2X)), R2X, ax=axis)
-    axis.set_xlabel("Components")
-    axis.set_ylabel("Gene R2X")
-    axis.set_yticks([0, .2, .4, .6, .8, 1])
-    axis.set_title("Variance Explained in Gene Data")
+def makeProteinListDistribution(axis):
+    components = 5
+    results, treatmentList, times = decomp_to_flipped_factors(components)
+
+    proteinFactors = results[1][0]
+    proteinList = proteinNames()
+
+    comp1Vals = []
+    comp2Vals = []
+    comp3Vals = []
+    comp4Vals = []
+    comp5Vals = []
+
+    for compVals in proteinFactors:
+        comp1Vals.append(compVals[0])
+        comp2Vals.append(compVals[1])
+        comp3Vals.append(compVals[2])
+        comp4Vals.append(compVals[3])
+        comp5Vals.append(compVals[4])
+
+    plt.figure(figsize=(70,10))
+
+
+
+    plt.scatter(proteinList, comp1Vals, label = 'Component 1', ax=axis)
+    plt.scatter(proteinList, comp2Vals, label = 'Component 2', ax=axis)
+    plt.scatter(proteinList, comp3Vals, label = 'Component 3',ax=axis)
+    plt.scatter(proteinList, comp4Vals, label = 'Component 4', ax=axis)
+    plt.scatter(proteinList, comp5Vals, label = 'Component 5', ax=axis)
+
+
+    plt.xticks(proteinList, proteinList, rotation='vertical')
+
+    plt.legend(loc=2, prop={'size': 20})
 
