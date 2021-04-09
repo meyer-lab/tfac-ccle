2247f4fcf488f22e6422a9447dd5e2091e5bd77f
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-04-09 08:15:30 -0700

    Cleanup (#200)
    * Clean
    
    * More clean
    
    * Clean
    
    * Clean
    
    * Fix

--- tfac/tests/test_factorization.py
+++ tfac/tests/test_factorization.py
@@ -1,20 +1,19 @@
 import numpy as np
 import pandas as pd
 from ..tensor import decomp_to_flipped_factors
-from ..Data_Mod import form_tensor
 from ..dataHelpers import ohsu_data
 from ..pseudoinvnorm import find_factors
 
 
 def test_check_size():
-    """Imports raw data, implements tensor decomposition and reverse projection. 
-    Checks factor sizes. 
+    """Imports raw data, implements tensor decomposition and reverse projection.
+    Checks factor sizes.
     Returns decomp and projection sizes."""
-    #Data Import
-    comps = 5 # Use 5 components
+    # Data Import
+    comps = 5  # Use 5 components
     _, _, _, _, _, RNAseq, _ = ohsu_data()
     RNAseq.drop("ensembl_gene_id", inplace=True, axis=1)
-    #Tensor Decomp & Reverse Projection
+    # Tensor Decomp & Reverse Projection
     result, treatment_list, times = decomp_to_flipped_factors(comps)
     P_gene, _, _, W_gene = find_factors(result, RNAseq, treatment_list, times)
     gene_recon = np.matmul(W_gene.T, P_gene)
