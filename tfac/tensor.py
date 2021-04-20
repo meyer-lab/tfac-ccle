f6d52093208aee337728421d2ecb45195c09dc5d
Linnet Chang <lchang21@ucla.edu>
2021-04-19 23:02:31 -0700

    update factorization function

--- tfac/tensor.py
+++ tfac/tensor.py
@@ -17,10 +17,10 @@ def R2X(reconstructed, original):
     return 1.0 - tl_var(reconstructed - original) / tl_var(original)
 
 
-#### Decomposition Methods ###################################################################
+###### Factorization #########################################################################
 
 
-def partial_tucker_decomp(tensor, mode_list, rank):
+def tensor_factor(protein, geneExp):
     """Perform Partial Tucker decomposition.
     -----------------------------------------------
     Input:
@@ -31,39 +31,28 @@ def partial_tucker_decomp(tensor, mode_list, rank):
         output[0]: core tensor
         output[1]: list of factor matrices
     """
-    pre_flip_result = partial_tucker(tensor, mode_list, rank, tol=1.0e-12)
-    result = flip_factors(pre_flip_result)
-    return result
-
-
-#### For R2X Plots ###########################################################################
-
-
-def find_R2X_partialtucker(tucker_output, orig):
-    """Compute R2X for the tucker decomposition."""
-    return R2X(mode_dot(tucker_output[0], tucker_output[1][0], 2), orig)
-
-
-###### To Flip Factors #########################################################################
-
-
-def flip_factors(tucker_output):
-    """For partial tucker OHSU factorization, flips protein and treatment/time factors if both negative for important values"""
-    for component in range(tucker_output[0].shape[2]):
+    component = 5
+    #Protein Factors
+    proteinFactors = partial_tucker(protein, [2], component, tol=1.0e-12)
+    for component in range(proteinFactors[0].shape[2]):
         av = 0.0
-        for i in range(tucker_output[0].shape[0]):
-            av += np.mean(tucker_output[0][i][:, component] ** 5)
+        for i in range(proteinFactors[0].shape[0]):
+            av += np.mean(proteinFactors[0][i][:, component] ** 5)
 
-        if av < 0 and tucker_output[1][0][:, component].mean() < 0:
-            tucker_output[1][0][:, component] *= -1
-            for j in range(tucker_output[0].shape[0]):
-                tucker_output[0][j][:, component] *= -1
-    return tucker_output
+        if av < 0 and proteinFactors[1][0][:, component].mean() < 0:
+            proteinFactors[1][0][:, component] *= -1
+            for j in range(proteinFactors[0].shape[0]):
+                proteinFactors[0][j][:, component] *= -1
+    #Gene Expression Factors
+    geneFactors = partial_tucker(geneExp, [2], component, tol=1.0e-12)
+    for component in range(geneFactors[0].shape[2]):
+        av = 0.0
+        for i in range(geneFactors[0].shape[0]):
+            av += np.mean(geneFactors[0][i][:, component] ** 5)
 
-
-def decomp_to_flipped_factors(components):
-    """"Parital Tucker decomposition and flipping factors.
-    Returns flipped factors, treatment list, and times."""
-    tensor, treatment_list, times = form_tensor()
-    result = partial_tucker_decomp(tensor, [2], components)
-    return result, treatment_list, times
+        if av < 0 and geneFactors[1][0][:, component].mean() < 0:
+            geneFactors[1][0][:, component] *= -1
+            for j in range(geneFactors[0].shape[0]):
+                geneFactors[0][j][:, component] *= -1
+    return proteinFactors, geneFactors
+     
