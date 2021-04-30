cd9c78e9eb6ab78ad06166d8dc8ac97c023c0c09
aarmey <aarmey@users.noreply.github.com>
2021-04-30 18:28:55 +0000

    autopep8 action fixes

--- tfac/figures/figure2.py
+++ tfac/figures/figure2.py
@@ -10,6 +10,7 @@ from ..Data_Mod import form_tensor
 from ..dataHelpers import importLINCSprotein, proteinNames
 from ..tensor import decomp_to_flipped_factors, partial_tucker_decomp
 
+
 def makeCompPlots(axis1, axis2, axis3):
     components = 5
     results, treatmentList, times = decomp_to_flipped_factors(components)
@@ -17,26 +18,25 @@ def makeCompPlots(axis1, axis2, axis3):
 
     componeDict = {}
 
-    #Initialize an empty dictionary of lists
+    # Initialize an empty dictionary of lists
     for treatment in treatmentList:
         componeDict[treatment] = []
 
-
     counter = 0
-    #Create a dictionary with treatments as keys and the treatment value list as the value
+    # Create a dictionary with treatments as keys and the treatment value list as the value
     for keyTreatment in componeDict:
         counter1 = 0
         for timeList in treatmentTime[counter]:
             componeDict[keyTreatment].append(timeList[0])
-            counter1+=1
-        counter+=1
+            counter1 += 1
+        counter += 1
 
-    counter=0
+    counter = 0
 
-    #Plot the times against 
+    # Plot the times against
     for treatment in treatmentList:
         sns.lineplot(x=times, y=componeDict[treatment], label=treatmentList[counter], ax=axis1, legend=True)
-        counter+=1
+        counter += 1
     axis1.set_title("Component 1 v.s. Times")
     axis1.set(xlabel='Times', ylabel='Component Values')
 
@@ -46,26 +46,25 @@ def makeCompPlots(axis1, axis2, axis3):
 
     compfourDict = {}
 
-    #Initialize an empty dictionary of lists
+    # Initialize an empty dictionary of lists
     for treatment in treatmentList:
         compfourDict[treatment] = []
 
-
     counter = 0
-    #Create a dictionary with treatments as keys and the treatment value list as the value
+    # Create a dictionary with treatments as keys and the treatment value list as the value
     for keyTreatment in compfourDict:
         counter1 = 0
         for timeList in treatmentTime[counter]:
             compfourDict[keyTreatment].append(timeList[3])
-            counter1+=1
-        counter+=1
+            counter1 += 1
+        counter += 1
 
     counter = 0
 
-    #Plot the times against 
+    # Plot the times against
     for treatment in treatmentList:
         sns.lineplot(x=times, y=compfourDict[treatment], label=treatmentList[counter], ax=axis2, legend=True)
-        counter+=1
+        counter += 1
     axis2.set_title("Component 4 v.s. Times")
     axis2.set(xlabel='Times', ylabel='Component Values')
 
@@ -75,28 +74,28 @@ def makeCompPlots(axis1, axis2, axis3):
 
     compfiveDict = {}
 
-    #Initialize an empty dictionary of lists
+    # Initialize an empty dictionary of lists
     for treatment in treatmentList:
         compfiveDict[treatment] = []
 
-
     counter = 0
-    #Create a dictionary with treatments as keys and the treatment value list as the value
+    # Create a dictionary with treatments as keys and the treatment value list as the value
     for keyTreatment in compfiveDict:
         counter1 = 0
         for timeList in treatmentTime[counter]:
             compfiveDict[keyTreatment].append(timeList[4])
-            counter1+=1
-        counter+=1
+            counter1 += 1
+        counter += 1
 
     counter = 0
 
-    #Plot the times against 
+    # Plot the times against
     for treatment in treatmentList:
         sns.lineplot(x=times, y=compfiveDict[treatment], label=treatmentList[counter], ax=axis3, legend=True)
-        counter+=1
+        counter += 1
     axis3.set_title("Component 5 v.s. Times")
     axis3.set(xlabel='Times', ylabel='Component Values')
+
 
 def makeHeatMap(axis):
     result, treatments, times = decomp_to_flipped_factors(5)
@@ -114,13 +113,6 @@ def makeHeatMap(axis):
     axis.set(xlabel='Component', ylabel='Proteins')
 
 
-
-
-
-
-
-
-
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     row = 2
@@ -135,5 +127,3 @@ def makeFigure():
 
     fig, ax = plt.subplots(2, 2)
     return f
-    
-    
--- tfac/figures/figure4.py
+++ tfac/figures/figure4.py
@@ -22,10 +22,10 @@ def makeFigure():
     # Add subplot labels
     subplotLabel(ax)
 
-
     makeProteinListDistribution(ax[0])
 
     return f
+
 
 def makeProteinListDistribution(ax):
     components = 5
@@ -52,8 +52,6 @@ def makeProteinListDistribution(ax):
     proteinComp4 = []
     proteinComp5 = []
 
-    
-
     proteinCounter1 = 0
     proteinCounter2 = 0
     proteinCounter3 = 0
@@ -65,67 +63,63 @@ def makeProteinListDistribution(ax):
             comp1Vals.append(compVals[0])
             comp1.append("Component 1")
             proteinComp1.append(proteinList[proteinCounter1])
-            proteinCounter1+=1
+            proteinCounter1 += 1
         if(compVals[1] < -.15 or compVals[1] > .15):
             comp2Vals.append(compVals[1])
             comp2.append("Component 2")
             proteinComp2.append(proteinList[proteinCounter2])
-            proteinCounter2+=1
+            proteinCounter2 += 1
         if(compVals[2] < -.15 or compVals[2] > .15):
             comp3Vals.append(compVals[2])
             comp3.append("Component 3")
             proteinComp3.append(proteinList[proteinCounter3])
-            proteinCounter3+=1
+            proteinCounter3 += 1
         if(compVals[3] < -.15 or compVals[3] > .15):
             comp4Vals.append(compVals[3])
             comp4.append("Component 4")
             proteinComp4.append(proteinList[proteinCounter4])
-            proteinCounter4+=1
+            proteinCounter4 += 1
         if(compVals[4] < -.15 or compVals[4] > .15):
             comp5Vals.append(compVals[4])
             comp5.append("Component 5")
             proteinComp5.append(proteinList[proteinCounter5])
-            proteinCounter5+=1
+            proteinCounter5 += 1
 
-    
-
-    sns.scatterplot(x=comp1, y=comp1Vals, ax=ax, color ='blue')
-    sns.scatterplot(x=comp2, y=comp2Vals, ax=ax, color = 'orange')
-    sns.scatterplot(x=comp3, y=comp3Vals, ax=ax, color = 'yellow')
-    sns.scatterplot(x=comp4, y=comp4Vals, ax=ax, color = 'green')
+    sns.scatterplot(x=comp1, y=comp1Vals, ax=ax, color='blue')
+    sns.scatterplot(x=comp2, y=comp2Vals, ax=ax, color='orange')
+    sns.scatterplot(x=comp3, y=comp3Vals, ax=ax, color='yellow')
+    sns.scatterplot(x=comp4, y=comp4Vals, ax=ax, color='green')
     sns.scatterplot(x=comp5, y=comp5Vals, ax=ax, color='red')
 
-    for line in range(0,len(comp1Vals)):
+    for line in range(0, len(comp1Vals)):
         if(line % 2 == 0):
             ax.text(comp1[line], comp1Vals[line], proteinList[line], horizontalalignment='right', size=6, color='black', weight='semibold')
         else:
             ax.text(comp1[line], comp1Vals[line], proteinList[line], horizontalalignment='left', size=6, color='black', weight='semibold')
 
-    for line in range(0,len(comp2Vals)):
+    for line in range(0, len(comp2Vals)):
         if(line % 2 == 0):
             ax.text(comp2[line], comp2Vals[line], proteinList[line], horizontalalignment='right', size=6, color='black', weight='semibold')
         else:
             ax.text(comp2[line], comp2Vals[line], proteinList[line], horizontalalignment='left', size=6, color='black', weight='semibold')
 
-    for line in range(0,len(comp3Vals)):
+    for line in range(0, len(comp3Vals)):
         if(line % 2 == 0):
             ax.text(comp3[line], comp3Vals[line], proteinList[line], horizontalalignment='right', size=6, color='black', weight='semibold')
         else:
             ax.text(comp3[line], comp3Vals[line], proteinList[line], horizontalalignment='left', size=6, color='black', weight='semibold')
 
-    for line in range(0,len(comp4Vals)):
+    for line in range(0, len(comp4Vals)):
         if(line % 2 == 0):
             ax.text(comp4[line], comp4Vals[line], proteinList[line], horizontalalignment='right', size=6, color='black', weight='semibold')
         else:
             ax.text(comp4[line], comp4Vals[line], proteinList[line], horizontalalignment='left', size=6, color='black', weight='semibold')
 
-    for line in range(0,len(comp5Vals)):
+    for line in range(0, len(comp5Vals)):
         if(line % 2 == 0):
             ax.text(comp5[line], comp5Vals[line], proteinList[line], horizontalalignment='right', size=6, color='black', weight='semibold')
         else:
             ax.text(comp5[line], comp5Vals[line], proteinList[line], horizontalalignment='left', size=6, color='black', weight='semibold')
-
-
 
     comp1Vals.clear()
     comp2Vals.clear()
@@ -156,12 +150,11 @@ def makeProteinListDistribution(ax):
             comp5Vals.append(compVals[4])
             comp5.append("Component 5")
 
-    sns.scatterplot(x=comp1, y=comp1Vals, ax=ax, color ='blue', alpha=.05)
-    sns.scatterplot(x=comp2, y=comp2Vals, ax=ax, color = 'orange', alpha=.05)
-    sns.scatterplot(x=comp3, y=comp3Vals, ax=ax, color = 'yellow', alpha=.05)
-    sns.scatterplot(x=comp4, y=comp4Vals, ax=ax, color = 'green', alpha=.05)
+    sns.scatterplot(x=comp1, y=comp1Vals, ax=ax, color='blue', alpha=.05)
+    sns.scatterplot(x=comp2, y=comp2Vals, ax=ax, color='orange', alpha=.05)
+    sns.scatterplot(x=comp3, y=comp3Vals, ax=ax, color='yellow', alpha=.05)
+    sns.scatterplot(x=comp4, y=comp4Vals, ax=ax, color='green', alpha=.05)
     sns.scatterplot(x=comp5, y=comp5Vals, ax=ax, color='red', alpha=.05)
-
 
     ax.set_title("Component Values for Proteins")
     ax.set(xlabel='Components', ylabel='Component Values')
