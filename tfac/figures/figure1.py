4dd17d04b2f16b310156a22a1a1cd0270bef876d
Farnaz Mohammadi <farnazmd95@gmail.com>
2021-12-21 17:55:10 -0800

    figure reorganization and clustergram (#216)
    * figure reorganization and clustergram
    
    * filtered insignificant proteins
    
    * centered colorbar to 0

--- tfac/figures/figure1.py
+++ tfac/figures/figure1.py
@@ -1,56 +1,28 @@
 """
-This creates Figure 1 - Partial Tucker R2X and Gene Projected Factors R2X.
+This creates Figure 1:
+(a) tensor factorization cartoon
+(b) tensor_svg.svg from the data folder
+(c) R2X of the whole data, including gene expressions and protein levels.
 """
-import numpy as np
-import pandas as pd
-import seaborn as sns
 from .figureCommon import subplotLabel, getSetup
-from tensorpack import Decomposition, perform_CP
+from tensorpack import Decomposition
 from tensorpack.plot import *
-from ..dataHelpers import form_tensor, proteinNames
+from ..dataHelpers import form_tensor
 
 
 def makeFigure():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
-    ax, f = getSetup((20, 20), (2, 4))
+    ax, f = getSetup((8, 10), (3, 1))
+    ax[0].axis("off")
+    ax[1].axis("off")
 
-    tensor, drugs, time = form_tensor()
-    ## With new figure making function in tensorpack
-    t = Decomposition(tensor, max_rr=7)
+    tensor, _, _ = form_tensor()
+    # perform tensor decomposition from tensorpack with 8 components
+    t = Decomposition(tensor, max_rr=8)
     t.perform_tfac()
-    tfacr2x(ax[0], t)
+    tfacr2x(ax[2], t)
 
-    tFac = perform_CP(tensor, r=6, tol=1e-6)
-    treatment = pd.DataFrame(tFac.factors[0], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=list(drugs))
-    time = pd.DataFrame(tFac.factors[1], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=time)
-    proteins1 = pd.DataFrame(tFac.factors[2][57367:57450], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=proteinNames()[0:83])
-    proteins2 = pd.DataFrame(tFac.factors[2][57450:57520], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=proteinNames()[83:153])
-    proteins3 = pd.DataFrame(tFac.factors[2][57520:57590], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=proteinNames()[153:223])
-    proteins4 = pd.DataFrame(tFac.factors[2][57590:], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=proteinNames()[223:])
-    genes = pd.DataFrame(tFac.factors[2][0:80], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)])
-
-    g1 = sns.heatmap(treatment, cmap="PRGn", center=0, xticklabels=True, yticklabels=True, cbar=False, vmin=-1.0, vmax=1.0, ax=ax[1])
-    g1.set_yticklabels(g1.get_yticklabels(), rotation = 0)
-    g1.set_title("treatments")
-    g2 = sns.heatmap(time, cmap="PRGn", center=0, yticklabels=True, cbar=True, vmin=-1.0, vmax=1.0, ax=ax[2])
-    g2.set_yticklabels(g2.get_yticklabels(), rotation = 0)
-    g2.set_title("time")
-    g3 = sns.heatmap(genes, cmap="PRGn", center=0, yticklabels=True, cbar=True, vmin=-1.0, vmax=1.0, ax=ax[3])
-    g3.set_yticklabels(g3.get_yticklabels(), rotation = 0)
-    g3.set_title("genes")
-    g4 = sns.heatmap(proteins1, cmap="PRGn", center=0, yticklabels=True, cbar=True, vmin=-0.01, vmax=0.01, ax=ax[4])
-    g4.set_yticklabels(g4.get_yticklabels(), rotation = 0)
-    g4.set_title("proteins")
-    g5 = sns.heatmap(proteins2, cmap="PRGn", center=0, yticklabels=True, cbar=False, vmin=-0.01, vmax=0.01, ax=ax[5])
-    g5.set_yticklabels(g5.get_yticklabels(), rotation = 0)
-    g5.set_title("proteins")
-    g6 = sns.heatmap(proteins3, cmap="PRGn", center=0, yticklabels=True, cbar=False, vmin=-0.01, vmax=0.01, ax=ax[6])
-    g6.set_yticklabels(g6.get_yticklabels(), rotation = 0)
-    g6.set_title("proteins")
-    g7 = sns.heatmap(proteins4, cmap="PRGn", center=0, yticklabels=True, cbar=False, vmin=-0.01, vmax=0.01, ax=ax[7])
-    g7.set_yticklabels(g7.get_yticklabels(), rotation = 0)
-    g7.set_title("proteins")
     # Add subplot labels
     subplotLabel(ax)
 
