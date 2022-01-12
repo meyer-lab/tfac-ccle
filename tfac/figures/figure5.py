81121b23e36c20875733d8c9972c31e47c5b9b61
farnazm <farnazmd95@gmail.com>
2022-01-12 09:10:31 -0800

    edits to figure 5

--- tfac/figures/figure5.py
+++ tfac/figures/figure5.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import h5py
 from tensorpack.figureCommon import getSetup
 from tensorpack.decomposition import Decomposition
-from tensorpack.cmtf import perform_CP
+from tensorpack.cmtf import perform_CP, reorient_factors
 from tensorpack.plot import tfacr2x, reduction
 
 path_here = dirname(dirname(__file__))
@@ -26,6 +26,7 @@ def import_data():
     """
     f = h5py.File(join(path_here, "data/drug_response/data.jld"), "r")
     d = np.array(f["data"][:])
+
     return d - np.mean(d)
 
 def makeFigure():
@@ -38,17 +39,20 @@ def makeFigure():
     reduction(ax[1], alldat)
 
     tFac = perform_CP(data, r=3)
+    tFac = reorient_factors(tFac)
     drug = pd.DataFrame(tFac.factors[0], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=drugs)
     concentrations = pd.DataFrame(tFac.factors[1], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=conc)
     times = pd.DataFrame(tFac.factors[2], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=time)
     G1SG2 = pd.DataFrame(tFac.factors[3], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=g1g2)
 
-    g1 = sns.heatmap(drug, cmap="PRGn", center=0, xticklabels=True, yticklabels=True, cbar=False, cbar_ax=ax[5], vmin=-1.0, vmax=1.0, ax=ax[2])
+    g1 = sns.heatmap(drug, cmap="PRGn", center=0, xticklabels=True, yticklabels=True, cbar=True, vmin=-1.0, vmax=1.0, ax=ax[2])
     g1.set_yticklabels(g1.get_yticklabels(), rotation = 0)
-    g2 = sns.heatmap(concentrations, cmap="PRGn", center=0, yticklabels=True, cbar=False, vmin=-1.0, vmax=1.0, ax=ax[3])
+    g2 = sns.heatmap(concentrations, cmap="PRGn", center=0, yticklabels=True, cbar=True, vmin=-1.0, vmax=1.0, ax=ax[3])
     g2.set_yticklabels(g2.get_yticklabels(), rotation = 0)
-    g3 = sns.heatmap(times, cmap="PRGn", center=0, yticklabels=True, cbar=False, vmin=-1.0, vmax=1.0, ax=ax[4])
-    g3.set_yticklabels(g3.get_yticklabels(), rotation = 0)
-    g4 = sns.heatmap(G1SG2, cmap="PRGn", center=0, yticklabels=True, cbar=False, vmin=-1.0, vmax=1.0, ax=ax[5])
+    ax[4].plot(time, times)
+    ax[4].set_xlabel("Time")
+    ax[4].set_ylabel("Component Weight")
+    ax[4].legend(["Cmp. 1", "Cmp. 2", "Cmp. 3", "Cmp. 4", "Cmp. 5"])
+    g4 = sns.heatmap(G1SG2, cmap="PRGn", center=0, yticklabels=True, cbar=True, vmin=-1.0, vmax=1.0, ax=ax[5])
     g4.set_yticklabels(g4.get_yticklabels(), rotation = 0)
     return f
