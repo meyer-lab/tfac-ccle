1e911984bae5697ef4266a5471093b57c1b8a946
Aaron Meyer <ameyer@ucla.edu>
2022-01-23 19:46:37 -0800

    Some adjustments

--- tfac/figures/figure2.py
+++ tfac/figures/figure2.py
@@ -19,24 +19,21 @@ def makeFigure():
 
     tensor, drugs, times = form_tensor()
 
-    tFac = perform_CP(tensor, r=4)
+    tFac = perform_CP(tensor, r=4, maxiter=2000, progress=True)
 
     treatment = pd.DataFrame(tFac.factors[0], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=drugs)
     tm = pd.DataFrame(tFac.factors[1], columns=[f"Cmp. {i}" for i in np.arange(1, tFac.rank + 1)], index=times)
 
-    g1 = sns.heatmap(treatment, cmap="PRGn", center=0, yticklabels=True, cbar=True, vmin=-1., vmax=1., ax=ax[0])
+    g1 = sns.heatmap(treatment, cmap="PRGn", center=0, yticklabels=True, cbar=True, ax=ax[0])
     g1.set_yticklabels(g1.get_yticklabels(), rotation=0)
     g1.set_title("treatment")
 
-    g2 = sns.heatmap(tm, cmap="PRGn", center=0, yticklabels=True, cbar=True, vmin=-1., vmax=1., ax=ax[1])
+    g2 = sns.heatmap(tm, cmap="PRGn", center=0, yticklabels=True, cbar=True, ax=ax[1])
     g2.set_yticklabels(g2.get_yticklabels(), rotation=0)
     g2.set_title("times")
 
     for i in range(2, 6):
         ax[i].axis("off")
-    # ? - subplot (b)
-
-    # components vs time - subplots (c-f)
 
     # Add subplot labels
     subplotLabel(ax)
