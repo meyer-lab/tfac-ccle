aa42e5e1f72c0c59b27a15f42254e8d3d2c02158
farnazm <farnazmd95@gmail.com>
2022-01-23 19:10:23 -0800

    forgot to push

--- tfac/figures/figClust.py
+++ tfac/figures/figClust.py
@@ -13,15 +13,15 @@ def clustergram_proteins_geneModules():
     """ Get a list of the axis objects and create a figure. """
     # Get list of axis objects
     tensor, _, _ = form_tensor()
-    tFac = perform_CP(tensor, r=6)
+    tFac = perform_CP(tensor, r=4)
 
-    proteins = pd.DataFrame(tFac.factors[2][:295], index=proteinNames(), columns=["comp1", "comp2", "comp3", "comp4", "comp5", "comp6"])
+    proteins = pd.DataFrame(tFac.factors[2][:295], index=proteinNames(), columns=["comp1", "comp2", "comp3", "comp4"])
     decreased_proteins = proteins.loc[((-0.1 >= proteins).any(1) | (proteins >= 0.1).any(1))]
     g = sns.clustermap(decreased_proteins, cmap="bwr", method="centroid", center=0, figsize=(14, 20))
     plt.savefig("output/clustergram_proteins.svg")
 
     RNAseq = pd.read_csv("tfac/data/ohsu/module_expression.csv", delimiter=",")
-    genes = pd.DataFrame(tFac.factors[2][295:], index=list(RNAseq["Unnamed: 0"]), columns=["comp1", "comp2", "comp3", "comp4", "comp5", "comp6"])
+    genes = pd.DataFrame(tFac.factors[2][295:], index=list(RNAseq["Unnamed: 0"]), columns=["comp1", "comp2", "comp3", "comp4"])
     decreased_genes = genes.loc[((-0.01 >= genes).any(1) | (genes >= 0.01).any(1))]
     g = sns.clustermap(decreased_genes, cmap="bwr", method="centroid", center=0, figsize=(14, 20))
     plt.savefig("output/clustergram_geneModules.svg")
