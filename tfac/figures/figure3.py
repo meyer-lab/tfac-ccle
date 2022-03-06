572e48a3953f65d9e66bbb9d2901d8c690a938a8
Farnaz Mohammadi <farnazmd95@gmail.com>
2022-03-05 23:55:54 -0800

    remove figclust (#241)

--- tfac/figures/figure3.py
+++ tfac/figures/figure3.py
@@ -18,9 +18,9 @@ def makeFigure():
     fac = cp_flip_sign(fac, 2)
     fac = cp_normalize(fac)
 
-    facZero = pd.DataFrame(fac.factors[0], columns=[f"{i}" for i in np.arange(1, fac.factors[0].shape[1] + 1)], index=ligand)
-    facOne = pd.DataFrame(fac.factors[1], columns=[f"{i}" for i in np.arange(1, fac.factors[1].shape[1] + 1)], index=ecm)
-    facTwo = pd.DataFrame(fac.factors[2], columns=[f"{i}" for i in np.arange(1, fac.factors[2].shape[1] + 1)], index=measurements)
+    facZero = pd.DataFrame(fac.factors[0], columns=[f"Cmp. {i}" for i in np.arange(1, fac.factors[0].shape[1] + 1)], index=ligand)
+    facOne = pd.DataFrame(fac.factors[1], columns=[f"Cmp. {i}" for i in np.arange(1, fac.factors[1].shape[1] + 1)], index=ecm)
+    facTwo = pd.DataFrame(fac.factors[2], columns=[f"Cmp. {i}" for i in np.arange(1, fac.factors[2].shape[1] + 1)], index=measurements)
 
     facZero = reorder_table(facZero)
     facOne = reorder_table(facOne)
@@ -28,8 +28,8 @@ def makeFigure():
 
     print("R2X: ", 1.0 - np.linalg.norm(tl.cp_to_tensor(fac) - tensor)**2.0 / np.linalg.norm(tensor)**2.0)
 
-    sns.heatmap(facZero.T, ax=ax[0], center=0)
-    sns.heatmap(facOne.T, ax=ax[1], center=0)
-    sns.heatmap(facTwo.T, ax=ax[2], center=0)
+    sns.heatmap(facZero.T, ax=ax[0], cmap="PRGn", center=0)
+    sns.heatmap(facOne.T, ax=ax[1], cmap="PRGn", center=0)
+    sns.heatmap(facTwo.T, ax=ax[2], cmap="PRGn", center=0)
     return f
 
