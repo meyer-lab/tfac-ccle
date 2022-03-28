a26136641ffd08934c2c8e8c85866fedd0db6e53
Farnaz Mohammadi <farnazmd95@gmail.com>
2022-03-28 13:02:44 -0700

    reverse plotHelpers

--- tfac/plotHelpers.py
+++ tfac/plotHelpers.py
@@ -12,7 +12,7 @@ from .dataHelpers import reorder_table
 
 def plot_heatmaps(tensor, ax):
     """ Plots all the components across all factors for the MEMA datasets. """
-    fac = parafac(tensor.to_numpy(), 5, n_iter_max=2000, linesearch=True, tol=1e-9) # tensor is xarray type
+    fac = parafac(tensor.to_numpy(), 5, n_iter_max=2000, linesearch=True, tol=1e-9)
     fac = cp_flip_sign(fac, 2)
     fac = cp_normalize(fac)
 
@@ -24,23 +24,17 @@ def plot_heatmaps(tensor, ax):
         facZero = reorder_table(facZero)
         sns.heatmap(facZero.T, ax=ax[ii], cmap="PRGn", center=0)
 
-def plot_components_MEMA(tensor, ax):
+def plot_components(tensor, ax):
     """ Plots most significant components separately in the supplementary figures. """
-    tFac = parafac(tensor.to_numpy(), 5, n_iter_max=2000, linesearch=True, tol=1e-9) # tensor is xarray type
+    tFac = parafac(tensor.to_numpy(), 5, n_iter_max=2000, linesearch=True, tol=1e-9)
     tFac = cp_flip_sign(tFac, 2)
     tFac = cp_normalize(tFac)
+
     labels = [f"Cmp. {i}" for i in np.arange(1, tFac.factors[0].shape[1] + 1)]
 
-    k = 0
     for ii in range(tensor.ndim):
         facZero = pd.DataFrame(tFac.factors[ii], columns=labels, index=tensor.coords[tensor.dims[ii]])
         facZero = reorder_table(facZero)
 
-        for c, col in enumerate(facZero.keys()):
-            feature = facZero[[col]]
-
-            feature_l_ind = feature.abs().nlargest(10, col).index
-
-            g0 = sns.heatmap(feature.loc[feature_l_ind].sort_values([col]), ax=ax[c + k], cmap="PRGn", center=0, vmin=-1, vmax=1)
-            g0.set_title(f"Proteins, {col}")
-        k += 5
+        g1 = sns.heatmap(facZero, ax=ax[ii], cmap="PRGn", center=0, vmin=-1, vmax=1)
+        g1.set_title(tensor.dims[ii])
