170c006dffd6d3b3702137b5cdadbdc66bc9590b
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2022-04-25 11:20:41 -0700

    Processing Done

--- tfac/dataHelpers.py
+++ tfac/dataHelpers.py
@@ -175,17 +175,51 @@ donorDict = {"1869": "RA",
 
 def process_RA_Tensor():
     """Structures all Rheumatoid Arthritis Synovial Fibroblast data into a usable tensor"""
-    donor_list = ["2759", "1869", "1931", "2159", "2586", "2645", "2708", "2759"]
+    RA_df = pd.DataFrame()
+    donor_list = ["1869", "1931", "2159", "2586", "2645", "2708", "2759"]
     rep_list = [1, 2]
+    stimulants = ["IL1-a", "TNF-a", "PolyIC"]
+
     for donor in donor_list:
         for rep in rep_list:
             file_name = "tfac/data/RA_SF/SF_Donor_" + donor + "_" + donorDict[donor] + "_Rep" + str(rep) + ".csv"
-            print("Hi")
-            a = pd.DataFrame()
-            print(a)
-            print(join(path_here, file_name))
-
             raw_data = pd.read_csv(join(path_here, file_name))
-            print(raw_data)
-            avg_data = raw_data.groupby(['Sample_Name']).mean().reset_index()
-            print(avg_data)
+            avg_data = raw_data.groupby(['Sample Name']).mean().reset_index()
+            
+            for index, row in avg_data.iterrows():
+                expString = row["Sample Name"].split(", ")
+                if len(expString) == 2:
+                    avg_data.loc[index, "Stimulant"] = expString[0]
+                    avg_data.loc[index, "Inhibitor"] = expString[1]
+                elif expString[0] == "bufferonly":
+                    avg_data.loc[index, "Stimulant"] = "Buffer"
+                    avg_data.loc[index, "Inhibitor"] = "Buffer"
+                elif len(row["Sample Name"].split("_")) == 2:
+                    avg_data.loc[index, "Stimulant"] = row["Sample Name"].split("_")[0]
+                    avg_data.loc[index, "Inhibitor"] = "Spike"
+            avg_data = avg_data.drop(['Sample Name', 'ExpPlate', 'LMXassayPlate'], axis=1)
+            cytokines = avg_data.iloc[:, 0: -2].columns
+
+            # Background (Spike and Otherwise) Subtraction
+            for stimulant in stimulants:
+                if stimulant in avg_data.columns:
+                    avg_data.loc[(avg_data["Stimulant"] == stimulant)][stimulant] == np.nan
+                spike_row = avg_data.loc[(avg_data["Stimulant"] == stimulant) & (avg_data["Inhibitor"] == "Spike")].reset_index().drop("index", axis=1).iloc[0, 0:-2]
+                basal_row = avg_data.loc[(avg_data["Stimulant"] == "nostim") & (avg_data["Inhibitor"] == "noinh")].reset_index().drop("index", axis=1).iloc[0, 0:-2]
+                for inh in avg_data.loc[(avg_data["Stimulant"] == stimulant)].Inhibitor.unique():
+                    stim_inh_row = avg_data.loc[(avg_data["Stimulant"] == stimulant) & (avg_data["Inhibitor"] == inh)].iloc[0, 0:-2]
+                    basal_spike_df = pd.concat([stim_inh_row - spike_row, basal_row], axis=1).transpose()
+                    avg_data.loc[(avg_data["Stimulant"] == stimulant) & (avg_data["Inhibitor"] == inh), cytokines] = basal_spike_df.max().to_frame().transpose().values
+            avg_data[cytokines] = np.log(avg_data[cytokines].values)
+            avg_data[cytokines] -= avg_data.loc[avg_data.Stimulant == "Buffer", cytokines].values
+            avg_data[cytokines] = avg_data[cytokines].clip(lower=0)
+            
+            avg_data["Donor"] = donor
+            avg_data["Replicate"] = rep
+            avg_data["Status"] = donorDict[donor]
+            avg_data = pd.melt(avg_data, id_vars=["Stimulant", "Inhibitor", "Donor", "Status", "Replicate"], value_vars=cytokines).rename({"variable": "Cytokine", "value": "Log MFI"}, axis=1)
+            RA_df = pd.concat([RA_df, avg_data], axis=0)
+
+    # Average Over Replicates
+    RA_df = RA_df.groupby(["Stimulant", "Inhibitor", "Donor", "Status", "Cytokine"])["Log MFI"].mean().reset_index()
+    return RA_df
