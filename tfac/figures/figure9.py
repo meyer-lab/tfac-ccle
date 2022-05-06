5318bab2db7dc61d3112b12d4adb509e11b8e6ee
eniohodzic <ehodzic@g.ucla.edu>
2022-05-06 15:45:22 -0700

    correlation index

--- tfac/figures/figure9.py
+++ tfac/figures/figure9.py
@@ -14,4 +14,21 @@ def makeFigure():
     HMEC240 = Tensor_LINCS_MEMA("hmec240l_ssc_Level4.tsv.xz")
     HMEC122 = Tensor_LINCS_MEMA("hmec122l_ssc_Level4.tsv.xz")
 
-    
+    repeat = 10
+    dataset = MCF10A
+
+    tensor = dataset.to_numpy()
+    Decomp = Decomposition(tensor, max_rr=5)
+    Decomp.perform_tfac()
+
+    for count in range(0,repeat):
+
+        tensor_c = np.moveaxis(tensor,m,0)
+        impute.chord_drop(tensor_c, drop=100)
+        tensor_c = np.moveaxis(tensor_c,0,m)
+        Decomp_miss = Decomposition(tensor_c, max_rr=5)
+        Decomp_miss.perform_tfac()
+
+        factors = Decomp.tfac.factors
+        factors_miss = Decomp_miss.tfac.factors
+
