e1ad6787ebd1240410edcf133655a6a14fa20702
Farnaz Mohammadi <farnazmd95@gmail.com>
2022-05-13 14:38:07 -0700

    figure 7 and 8 integrated MEMA heatmap and r2x (#267)
    * plot
    
    * plot edits
    
    * Delete settings.json
    
    * Update figure8.py

--- tfac/figures/figure7.py
+++ tfac/figures/figure7.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+from tensorly.cp_tensor import cp_flip_sign
 import seaborn as sns
 from tensorpack import Decomposition
 from .common import subplotLabel, getSetup
@@ -10,18 +11,19 @@ from ..dataHelpers import integrate_MEMA, reorder_table
 
 def makeFigure():
     # Get list of axis objects
-    ax, f = getSetup((30, 20), (4, 1))
+    ax, f = getSetup((30, 25), (3, 1))
 
     MEMA = integrate_MEMA()
     tc = Decomposition(MEMA.to_numpy(), max_rr=11)
     tc.perform_tfac()
+    fac = tc.tfac[-1]
+    fac = cp_flip_sign(fac, 2)
+    labels = [str(i) for i in np.arange(1, fac.factors[0].shape[1] + 1)]
 
-    labels = [str(i) for i in np.arange(1, tc.tfac[-1].factors[0].shape[1] + 1)]
-
-    for ii in range(MEMA.ndim):
-        facZero = pd.DataFrame(tc.tfac[-1].factors[ii], columns=labels, index=MEMA.coords[MEMA.dims[ii]])
+    for ii in range(1, 4):
+        facZero = pd.DataFrame(fac.factors[ii], columns=labels, index=MEMA.coords[MEMA.dims[ii]])
         facZero = reorder_table(facZero)
-        sns.heatmap(facZero.T, ax=ax[ii], cmap="PRGn", center=0)
+        sns.heatmap(facZero.T, ax=ax[ii-1], cmap="PRGn", center=0)
 
     subplotLabel(ax)
     return f
